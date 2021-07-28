e40dcca363ea697aaf493d6cdfaf512286a7374e
Christopher Mooney <chris@dod.net>
2021-07-28 15:37:10 -0400

    cmooney-20210728: Adds RWA PIP addresses to test file (#67)
    * cmooney-20210728: Adds RWA PIP addresses to test file
    
    * optimizations needs to be on

--- test-dssspell.sh
+++ test-dssspell.sh
@@ -3,7 +3,7 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "kovan" ]] || { echo "Please set a kovan ETH_RPC_URL"; exit 1; }
 
-export DAPP_BUILD_OPTIMIZE=0
+export DAPP_BUILD_OPTIMIZE=1
 export DAPP_BUILD_OPTIMIZE_RUNS=1
 export DAPP_LIBRARIES=' lib/dss-exec-lib/src/DssExecLib.sol:DssExecLib:0x3644A28AA8204d09A1A0E423F7aC2ACaFf5b8bb3'
 export DAPP_LINK_TEST_LIBRARIES=0
