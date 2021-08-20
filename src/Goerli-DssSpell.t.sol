51dc071319fe8f846acad224b527e3775354a66d
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-08-20 08:21:59 -0300

    Test with deployed spell

--- src/Goerli-DssSpell.t.sol
+++ src/Goerli-DssSpell.t.sol
@@ -248,8 +248,8 @@ contract DssSpellTest is DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1628884736,                 // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0xf9E943e1c9fbE01d7Ad2eB9B987a49c5de8FD9ce),        // populate with deployed spell if deployed
+            deployed_spell_created:         1629458121,                 // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,              // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
