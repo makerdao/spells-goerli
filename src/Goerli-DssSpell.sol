25fa0a3739ceb74dbd43d9717ee757628960d9d4
Henrique Barcelos <hbarcelos909@gmail.com>
2021-11-30 15:05:55 -0300

    WIP: updated spells and test files

--- src/Goerli-DssSpell.sol
+++ src/Goerli-DssSpell.sol
@@ -20,7 +20,7 @@ pragma experimental ABIEncoderV2;
 
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
-import { VatAbstract, LerpFactoryAbstract, SpotAbstract} from "dss-interfaces/Interfaces.sol";
+import { VatAbstract, LerpFactoryAbstract, SpotAbstract, IlkRegistryAbstract } from "dss-interfaces/Interfaces.sol";
 
 contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
@@ -45,22 +45,21 @@ contract DssSpellAction is DssAction {
 
     // --- Rates ---
     uint256 constant ZERO_PCT_RATE           = 1000000000000000000000000000;
+    uint256 constant ZERO_ONE_PCT_RATE       = 1000000000031693947650284507;
+    uint256 constant ONE_PCT_RATE            = 1000000000315522921573372069;
     uint256 constant ONE_FIVE_PCT_RATE       = 1000000000472114805215157978;
+    uint256 constant TWO_PCT_RATE            = 1000000000627937192491029810;
+    uint256 constant TWO_FIVE_PCT_RATE       = 1000000000782997609082909351;
+    uint256 constant TWO_SEVEN_FIVE_PCT_RATE = 1000000000860244400048238898;
+    uint256 constant THREE_PCT_RATE          = 1000000000937303470807876289;
+    uint256 constant FOUR_PCT_RATE           = 1000000001243680656318820312;
+    uint256 constant SIX_PCT_RATE            = 1000000001847694957439350562;
+    uint256 constant SIX_FIVE_PCT_RATE       = 1000000001996917783620820123;
 
     // --- Math ---
-    uint256 constant MILLION                 = 10 ** 6;
-    uint256 constant RAD                     = 10 ** 45;
-
-    // --- WBTC-C ---
-    address constant MCD_JOIN_WBTC_C        = 0xe15E69F10E1A362F69d9672BFeA20B75CFf8574A;
-    address constant MCD_CLIP_WBTC_C        = 0xDa3cd88f5FF7D2B9ED6Ab171C8218421916B6e10;
-    address constant MCD_CLIP_CALC_WBTC_C   = 0xD26B140fdaA11c23b09230c24cBe71f456AC7ab6;
-
-    // --- PSM-GUSD-A ---
-    address constant MCD_JOIN_PSM_GUSD_A      = 0x4115fDa246e2583b91aD602213f2ac4fC6E437Ca; // AuthGemJoin8
-    address constant MCD_CLIP_PSM_GUSD_A      = 0x7A58fF23D5437C99b44BB02D7e24213D6dA20DFa;
-    address constant MCD_CLIP_CALC_PSM_GUSD_A = 0xE99bd8c56d7B9d90A36C8a563a4CA375b144dD94;
-    address constant MCD_PSM_GUSD_A           = 0x3B2dBE6767fD8B4f8334cE3E8EC3E2DF8aB3957b;
+    uint256 constant MILLION = 10 ** 6;
+    uint256 constant BILLION = 10 ** 9;
+    uint256 constant RAD     = 10 ** 45;
 
     function _add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, "DssSpellAction-add-overflow");
@@ -71,117 +70,66 @@ contract DssSpellAction is DssAction {
 
     function actions() public override {
 
-        // --- 2021-11-26 Weekly Executive ---
+        // --- 2021-12-03 Weekly Executive ---
 
-        // ----------------------------- Debt ceiling updates -----------------------------
-        // Increase the Aave D3M Maximum Debt Ceiling to 100 million
-        // https://vote.makerdao.com/polling/QmZhvNu5?network=mainnet#poll-detail
-        // https://forum.makerdao.com/t/proposal-to-increase-the-d3m-dc-from-50m-to-100m/11737
+        // ----------------------------- Rates updates -----------------------------
+        // Increase the ETH-A Stability Fee from 2.5% to 2.75%
+        DssExecLib.setIlkStabilityFee("ETH-A", TWO_SEVEN_FIVE_PCT_RATE, true);
 
-        //DssExecLib.setIlkAutoLineDebtCeiling("DIRECT-AAVEV2-DAI", 100 * MILLION); // Not supported on Goerli
+        // Increase the ETH-B Stability Fee from 6.0% to 6.5%
+        DssExecLib.setIlkStabilityFee("ETH-B", SIX_FIVE_PCT_RATE, true);
 
-        //  ----------------- Increase the Surplus Buffer via Lerp -----------------------
-        //  https://vote.makerdao.com/polling/QmUqfZRv?network=mainnet#poll-detail
-        DssExecLib.linearInterpolation({
-            _name:      "Increase SB - 20211126",
-            _target:    DssExecLib.vow(),
-            _what:      "hump",
-            _startTime: block.timestamp,
-            _start:     60 * MILLION * RAD,
-            _end:       90 * MILLION * RAD,
-            _duration:  210 days
-        });
+        // Increase the LINK-A Stability Fee from 1.5% to 2.5%
+        DssExecLib.setIlkStabilityFee("LINK-A", TWO_FIVE_PCT_RATE, true);
 
-        // ----------------------------- Collateral Onboarding ----------------------------
-        //  Add WBTC-C as a new Vault Type
-        //  https://vote.makerdao.com/polling/QmdVYMRo?network=mainnet#poll-detail (WBTC-C Onboarding)
-        //  https://forum.makerdao.com/t/proposed-risk-parameters-for-wbtc-c-vault-type/11707
-        //  https://forum.makerdao.com/t/signal-request-new-vault-type-for-wbtc-with-a-higher-lr/11579
+        // Increase the MANA-A Stability Fee from 3.0% to 6.0%
+        DssExecLib.setIlkStabilityFee("MANA-A", SIX_PCT_RATE, true);
 
-        // WBTC
-        address WBTC     = DssExecLib.getChangelogAddress("WBTC");
-        address PIP_WBTC = DssExecLib.getChangelogAddress("PIP_WBTC");
+        // Increase the UNI-A Stability Fee from 1.0% to 3.0%
+        DssExecLib.setIlkStabilityFee("UNI-A", THREE_PCT_RATE, true);
 
-        DssExecLib.addNewCollateral(
-            CollateralOpts({
-                ilk:                   "WBTC-C",
-                gem:                   WBTC,
-                join:                  MCD_JOIN_WBTC_C,
-                clip:                  MCD_CLIP_WBTC_C,
-                calc:                  MCD_CLIP_CALC_WBTC_C,
-                pip:                   PIP_WBTC,
-                isLiquidatable:        true,
-                isOSM:                 true,
-                whitelistOSM:          true,
-                ilkDebtCeiling:        100 * MILLION,
-                minVaultAmount:        7500,
-                maxLiquidationAmount:  25 * MILLION,
-                liquidationPenalty:    1300,                // 13% penalty fee
-                ilkStabilityFee:       ONE_FIVE_PCT_RATE,   // 1.5% stability fee
-                startingPriceFactor:   12000,               // Auction price begins at 120% of oracle
-                breakerTolerance:      5000,                // Allows for a 50% hourly price drop before disabling liquidations
-                auctionDuration:       90 minutes,
-                permittedDrop:         4000,                // 40% price drop before reset
-                liquidationRatio:      17500,               // 175% collateralization
-                kprFlatReward:         300,                 // 300 Dai
-                kprPctReward:          10                   // 0.1%
-            })
-        );
-        DssExecLib.setStairstepExponentialDecrease(MCD_CLIP_CALC_WBTC_C, 90 seconds, 9900);
-        DssExecLib.setIlkAutoLineParameters("WBTC-C", 1000 * MILLION, 100 * MILLION, 8 hours);
+        // Increase the GUSD-A Stability Fee from 0.0% to 1.0%
+        DssExecLib.setIlkStabilityFee("GUSD-A", ONE_PCT_RATE, true);
 
-        DssExecLib.setChangelogAddress("MCD_JOIN_WBTC_C", MCD_JOIN_WBTC_C);
-        DssExecLib.setChangelogAddress("MCD_CLIP_WBTC_C", MCD_CLIP_WBTC_C);
-        DssExecLib.setChangelogAddress("MCD_CLIP_CALC_WBTC_C", MCD_CLIP_CALC_WBTC_C);
+        // Increase the UNIV2DAIETH-A Stability Fee from 1.5% to 2.0%
+        DssExecLib.setIlkStabilityFee("UNIV2DAIETH-A", TWO_PCT_RATE, true);
 
-        // Add a GUSD Peg Stability Module
-        // https://vote.makerdao.com/polling/QmayeEjz?network=mainnet#poll-detail
-        // https://forum.makerdao.com/t/proposal-gusd-psm/11148
-        // https://forum.makerdao.com/t/psm-gusd-a-collateral-onboarding-risk-evaluation/11559
+        // Increase the UNIV2WBTCETH-A Stability Fee from 2.5% to 3.0%
+        DssExecLib.setIlkStabilityFee("UNIV2WBTCETH-A", THREE_PCT_RATE, true);
 
-        // GUSD
-        address GUSD     = DssExecLib.getChangelogAddress("GUSD");
-        address PIP_GUSD = DssExecLib.getChangelogAddress("PIP_GUSD");
+        // Increase the UNIV2USDCETH-A Stability Fee from 2.0% to 2.5%
+        DssExecLib.setIlkStabilityFee("UNIV2USDCETH-A", TWO_FIVE_PCT_RATE, true);
 
-        //GUSD PSM
-        DssExecLib.authorize(MCD_JOIN_PSM_GUSD_A, MCD_PSM_GUSD_A);
+        // Increase the UNIV2UNIETH-A Stability Fee from 2.0% to 4.0%
+        DssExecLib.setIlkStabilityFee("UNIV2UNIETH-A", FOUR_PCT_RATE, true);
 
-        DssExecLib.addNewCollateral(CollateralOpts({
-            ilk:                   "PSM-GUSD-A",
-            gem:                   GUSD,
-            join:                  MCD_JOIN_PSM_GUSD_A,
-            clip:                  MCD_CLIP_PSM_GUSD_A,
-            calc:                  MCD_CLIP_CALC_PSM_GUSD_A,
-            pip:                   PIP_GUSD,
-            isLiquidatable:        false,
-            isOSM:                 false,
-            whitelistOSM:          false,
-            ilkDebtCeiling:        10 * MILLION,
-            minVaultAmount:        0,
-            maxLiquidationAmount:  0,
-            liquidationPenalty:    1300,
-            ilkStabilityFee:       ZERO_PCT_RATE,
-            startingPriceFactor:   10500,
-            breakerTolerance:      9500, // Allows for a 5% hourly price drop before disabling liquidations
-            auctionDuration:       220 minutes,
-            permittedDrop:         9000,
-            liquidationRatio:      10000,
-            kprFlatReward:         300,
-            kprPctReward:          10 // 0.1%
-        }));
-        DssExecLib.setStairstepExponentialDecrease(MCD_CLIP_CALC_PSM_GUSD_A, 120 seconds, 9990);
-        DssExecLib.setIlkAutoLineParameters("PSM-GUSD-A", 10 * MILLION, 10 * MILLION, 24 hours);
+        // Decrease the GUNIV3DAIUSDC1-A Stability Fee from 0.5% to 0.1%
+        DssExecLib.setIlkStabilityFee("GUNIV3DAIUSDC1-A", ZERO_ONE_PCT_RATE, true);
 
-        DssExecLib.setValue(MCD_PSM_GUSD_A, "tin", 0);
-        DssExecLib.setValue(MCD_PSM_GUSD_A, "tout", 0);
+        // ----------------------------- Debt Ceiling updates -----------------------------
+        // Increase the WBTC-A Maximum Debt Ceiling (line) from 1.5 billion DAI to 2 billion DAI
+        // Increase the WBTC-A Target Available Debt (gap) from 60 million DAI to 80 million DAI
+        DssExecLib.setIlkAutoLineParameters("WBTC-A", 2 * BILLION, 80 * MILLION, 6 hours);
 
-        DssExecLib.setChangelogAddress("MCD_JOIN_PSM_GUSD_A", MCD_JOIN_PSM_GUSD_A);
-        DssExecLib.setChangelogAddress("MCD_CLIP_PSM_GUSD_A", MCD_CLIP_PSM_GUSD_A);
-        DssExecLib.setChangelogAddress("MCD_CLIP_CALC_PSM_GUSD_A", MCD_CLIP_CALC_PSM_GUSD_A);
-        DssExecLib.setChangelogAddress("MCD_PSM_GUSD_A", MCD_PSM_GUSD_A);
+        // Increase the Dust Parameter from 30,000 DAI to 40,000 DAI for the ETH-B
+        DssExecLib.setIlkMinVaultAmount("ETH-B", 40000);
+
+        // Increase the Dust Parameter from 10,000 DAI to 15,000 DAI for all vault-types excluding ETH-B and ETH-C
+        IlkRegistryAbstract ilkRegistry = IlkRegistryAbstract(DssExecLib.reg());
+        bytes32[] memory ilks = ilkRegistry.list();
+
+        for (uint256 i = 0; i < ilks.length; i++) {
+            (, , uint256 ilkClass, , , , ,) = ilkRegistry.info(ilks[i]);
+            bool isRWA = ilkClass == 3;
+
+            // Do not change the dust for RWAs
+            if (!isRWA && ilks[i] != "ETH-B" && ilks[i] != "ETH-C") {
+                DssExecLib.setIlkMinVaultAmount(ilks[i], 15000);
+            }
+        }
 
         // Changelog version
-        DssExecLib.setChangelogVersion("1.9.11");
+        DssExecLib.setChangelogVersion("1.9.12"); // ?????
     }
 }
 
