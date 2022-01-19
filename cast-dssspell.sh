33cf988bd33c8c773ae0d6d9177901c0d1c1ef7f
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2022-01-19 20:11:25 +0000

    apply @godsflaw review changes

--- cast-dssspell.sh
+++ cast-dssspell.sh
@@ -7,13 +7,13 @@ CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
 MCD_ADM=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "MCD_ADM")")")
 MCD_GOV=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "MCD_GOV")")")
 MCD_IOU=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "MCD_IOU")")")
-TEN_THOUSAND=$(seth --to-wei 10000 ether)
+MKR=$(seth --to-wei 10001 ether)
 
 if [[ -z "$1" ]]; then
     echo "Please specify the Goerli Spell Address"
 else
-    seth send "$MCD_GOV" 'approve(address,uint256)' "$MCD_ADM" "$TEN_THOUSAND"
-    seth send "$MCD_ADM" 'lock(uint256)' "$TEN_THOUSAND"
+    seth send "$MCD_GOV" 'approve(address,uint256)' "$MCD_ADM" "$MKR"
+    seth send "$MCD_ADM" 'lock(uint256)' "$MKR"
     
     seth send "$MCD_ADM" 'vote(address[] memory)' ["$spell"]
     seth send "$MCD_ADM" 'lift(address)' "$spell"
@@ -21,8 +21,8 @@ else
     
     sleep 60s
 
-    seth send "$MCD_IOU" 'approve(address,uint256)' "$MCD_ADM" "$TEN_THOUSAND"
-    seth send "$MCD_ADM" 'free(uint256)' "$TEN_THOUSAND"
+    seth send "$MCD_IOU" 'approve(address,uint256)' "$MCD_ADM" "$MKR"
+    seth send "$MCD_ADM" 'free(uint256)' "$MKR"
 
     sleep 60s
     
