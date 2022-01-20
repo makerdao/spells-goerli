bfa8e3f7d7b2b209d89742ef52a878b53a43812f
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-01-20 12:54:49 -0700

    add spell address and timestamp

--- src/Goerli-DssSpell.t.base.sol
+++ src/Goerli-DssSpell.t.base.sol
@@ -198,8 +198,8 @@ contract GoerliDssSpellTestBase is Config, DSTest, DSMath {
         // Test for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         1642187279,        // use get-created-timestamp.sh if deployed
+            deployed_spell:                 address(0x7E8D8a08D84dBE33e8C57dFB81e7779a3Cc55637),        // populate with deployed spell if deployed
+            deployed_spell_created:         1642706079,        // use get-created-timestamp.sh if deployed
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
