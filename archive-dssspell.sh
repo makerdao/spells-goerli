67f4759464e495f68663979546412fbaaf88d115
Brian McMichael <brian@brianmcmichael.com>
2022-01-20 15:50:23 -0500

    Update archive spell script and makefile

--- archive-dssspell.sh
+++ archive-dssspell.sh
@@ -5,12 +5,13 @@ set -e
 if [[ -z "$1" ]]; then
   echo "You must provide a name option to name the directory"
 else
-  mkdir "./archive/$name"
-  mkdir "./archive/$name/test"
-  cp "./src/Goerli-DssSpell.sol" "./archive/$name"
-  cp "./src/Goerli-DssSpellCollateralOnboarding.sol" "./archive/$name"
-  cp "./src/Goerli-DssSpell.t.sol" "./archive/$name"
-  cp "./src/Goerli-DssSpell.t.base.sol" "./archive/$name"
-  cp ./src/test/* "./archive/$name/test"
-  echo "Spell, tests and base copied to archive directory"
+  rm -rf "./archive/$1-DssSpell"
+  mkdir "./archive/$1-DssSpell"
+  mkdir "./archive/$1-DssSpell/test"
+  cp "./src/Goerli-DssSpell.sol" "./archive/$1-DssSpell"
+  cp "./src/Goerli-DssSpellCollateralOnboarding.sol" "./archive/$1-DssSpell"
+  cp "./src/Goerli-DssSpell.t.sol" "./archive/$1-DssSpell"
+  cp "./src/Goerli-DssSpell.t.base.sol" "./archive/$1-DssSpell"
+  cp ./src/test/* "./archive/$1-DssSpell/test"
+  echo "Spell, tests and base copied to archive directory $1-DssSpell"
 fi
