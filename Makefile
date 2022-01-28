96c77292b1d5de899a6afef78b0e835ad9ac5694
Nazzareno Massari <nas@naszam.co>
2022-01-28 20:15:54 +0000

    `wards.sh`

--- Makefile
+++ Makefile
@@ -8,3 +8,4 @@ test-forge      :; ./test-dssspell-forge.sh match="$(match)" block="$(block)"
 deploy          :; make && dapp create DssSpell | xargs ./verify.py DssSpell
 flatten         :; hevm flatten --source-file "src/Goerli-DssSpell.sol" > out/flat.sol
 archive-spell   :; ./archive-dssspell.sh "$(if $(date),$(date),$(shell date +'%Y-%m-%d'))"
+wards           :; ./wards.sh $(target)
---
+++ wards.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$ETH_RPC_URL" ]] || { echo "Please set a ETH_RPC_URL"; exit 1; }
+
+[[ "$1" ]] || { echo "Please specify the ChainLog Key in ASCII to inspect"; exit 1; }
+
+CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
+KEY=$(seth --to-bytes32 "$(seth --from-ascii "$1")")
+TARGET=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$KEY")
+
+echo -e "Network: $(seth chain)"
+LIST=$(seth call "$CHANGELOG" 'list()(bytes32[])')
+for key in $(echo -e "$LIST" | sed "s/,/ /g")
+do
+        ADDRESS=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$key")
+        WARDS=$(seth call "$ADDRESS" 'wards(address)(uint256)' "$TARGET" 2>/dev/null) || continue
+        [[ "$WARDS" == "1" ]] && seth --to-ascii "$key"
+done
