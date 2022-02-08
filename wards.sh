ccbd004c38d635157336265e2c50a0e7a0160dc4
Nazzareno Massari <nas@naszam.co>
2022-02-08 21:09:46 +0000

    improve gas fee spikes check

--- wards.sh
+++ wards.sh
@@ -6,8 +6,10 @@ set -e
 [[ "$1" ]] || { echo "Please specify the ChainLog Key in ASCII to inspect"; exit 1; }
 
 ### Override maxFeePerGas to avoid spikes
-[[ $ETH_GAS_PRICE && $ETH_GAS_PRICE -lt $(seth basefee) ]]
-export ETH_GAS_PRICE=$((ETH_GAS_PRICE + $(seth --to-wei 200 gwei)))
+BASE_FEE=$(seth basefee)
+if [ -n "$ETH_GAS_PRICE" ] && [ "$ETH_GAS_PRICE" -lt "$BASE_FEE" ]; then
+        ETH_GAS_PRICE=$((BASE_FEE * 3))
+fi
 
 CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
 KEY=$(seth --to-bytes32 "$(seth --from-ascii "$1")")
