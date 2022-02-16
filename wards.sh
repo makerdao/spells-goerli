03de9c7b265734b5fe1b6ea117c68db529e22b26
Nazzareno Massari <nas@naszam.co>
2022-02-16 01:44:21 +0000

    capitalization and naming

--- wards.sh
+++ wards.sh
@@ -6,28 +6,28 @@ set -e
 [[ "$1" ]] || { echo "Please specify the Target Address or ChainLog Key (ASCII) to inspect"; exit 1; }
 
 ### Override maxFeePerGas to avoid spikes
-BASE_FEE=$(seth basefee)
-[[ -n "$ETH_GAS_PRICE" ]] && ethGasPriceLtBaseFee=$(echo "$ETH_GAS_PRICE < $BASE_FEE" | bc)
-[[ "$ethGasPriceLtBaseFee" == 1 ]] && export "ETH_GAS_PRICE=$(echo "$BASE_FEE * 3" | bc)"
+baseFee=$(seth basefee)
+[[ -n "$ETH_GAS_PRICE" ]] && ethGasPriceLtBaseFee=$(echo "$ETH_GAS_PRICE < $baseFee" | bc)
+[[ "$ethGasPriceLtBaseFee" == 1 ]] && export "ETH_GAS_PRICE=$(echo "$baseFee * 3" | bc)"
 
 CHANGELOG=0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F
 
 if [[ "$1" =~ 0x* ]]; then
-    TARGET=$1
+    target=$1
 else
     KEY=$(seth --to-bytes32 "$(seth --from-ascii "$1")")
-    TARGET=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$KEY")
+    target=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$KEY")
 fi
 
 echo -e "Network: $(seth chain)"
-LIST=$(seth call "$CHANGELOG" 'list()(bytes32[])')
-for key in $(echo -e "$LIST" | sed "s/,/ /g")
+list=$(seth call "$CHANGELOG" 'list()(bytes32[])')
+for key in $(echo -e "$list" | sed "s/,/ /g")
 do
-    KEY=$(seth --to-ascii "$key" | sed 's/\x0/ /g')
-    ADDRESS=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$key")
-    WARDS=$(seth call "$ADDRESS" 'wards(address)(uint256)' "$TARGET" 2>/dev/null) || continue
-    [[ "$WARDS" == "1" ]] && echo "$KEY"
-    SOURCE=$(seth call "$ADDRESS" 'src()(address)' 2>/dev/null) || continue
-    SWARDS=$(seth call "$SOURCE" 'wards(address)(uint256)' "$TARGET" 2>/dev/null) || continue
-    [[ "$SWARDS" == "1" ]] && echo "source of $KEY"
+    contractName=$(seth --to-ascii "$key" | sed 's/\x0/ /g')
+    contract=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$key")
+    wards=$(seth call "$contract" 'wards(address)(uint256)' "$target" 2>/dev/null) || continue
+    [[ "$wards" == "1" ]] && echo "$contractName"
+    src=$(seth call "$contract" 'src()(address)' 2>/dev/null) || continue
+    srcWards=$(seth call "$src" 'wards(address)(uint256)' "$target" 2>/dev/null) || continue
+    [[ "$srcWards" == "1" ]] && echo "source of $contractName"
 done
