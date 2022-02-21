f88d1dff6238131f9486cffb5f7714bbc5155b4f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-02-21 17:43:16 -0300

    Some changes to cast script (#49)
    Co-authored-by: Nazzareno Massari <nas@naszam.co>

--- cast-dssspell.sh
+++ cast-dssspell.sh
@@ -10,10 +10,7 @@ MCD_GOV=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-byt
 MCD_IOU=$(seth call "$CHANGELOG" 'getAddress(bytes32)(address)' "$(seth --to-bytes32 "$(seth --from-ascii "MCD_IOU")")")
 
 ### Data
-HAT_THRESHOLD=$(seth --to-wei 100000 ETH)
-CONTI=1
-balanceGOV=$(seth call "$MCD_GOV" 'balanceOf(address)(uint256)' "$ETH_FROM")
-balanceIOU=$(seth call "$MCD_IOU" 'balanceOf(address)(uint256)' "$ETH_FROM")
+DESIRED_HAT_APPROVALS=$(seth --to-wei 100000 ETH)
 hat=$(seth call "$MCD_ADM" 'hat()(address)')
 approvals=$(seth call "$MCD_ADM" 'approvals(address)(uint256)' "$hat")
 deposits=$(seth call "$MCD_ADM" 'deposits(address)(uint256)' "$ETH_FROM")
@@ -22,30 +19,19 @@ if [[ -z "$1" ]];
 then
     echo "Please specify the Goerli Spell Address"
 else
-    ### Check Approvals and Deposits to Lock MKR
-    approvalsGeDeposits=$(echo "$approvals >= $deposits" | bc)
-    if [[ "$approvalsGeDeposits" == 1 ]]; then
-        ### Check Approvals against Hat Threshold to set Gap
-        gap=0
-        approvalsLtHatThreshold=$(echo "$approvals < $HAT_THRESHOLD" | bc)
-        [[ "$approvalsLtHatThreshold" == 1 ]] && gap=$(echo "$HAT_THRESHOLD - $approvals" | bc)
+    target=$DESIRED_HAT_APPROVALS
+    if [[ "$(echo "$approvals + 1 >= $target" | bc)" == 1 ]]; then
+        target=$(echo "$approvals + 1" | bc)
+    fi
 
-        delta=$(echo "$approvals - $deposits + $gap" | bc)
-        lockAmt=$(echo "$delta + $CONTI" | bc)
+    if [[ "$(echo "$deposits < $target" | bc)" == 1 ]]; then
+        lockAmt=$(echo "$target - $deposits" | bc)
+        [[ "$(echo "$(seth call "$MCD_GOV" 'balanceOf(address)(uint256)' "$ETH_FROM") >= $lockAmt" | bc)" == 1 ]] || { echo "$ETH_FROM: Insufficient MKR Balance"; exit 1; }
 
-        ### Check MKR Balance
-        balanceGOVGeLockAmt=$(echo "$balanceGOV >= $lockAmt" | bc)
-        [[ "$balanceGOVGeLockAmt" == 1 ]] || { echo "$ETH_FROM: Insufficient MKR Balance"; exit 1; }
+        seth send "$MCD_GOV" 'approve(address,uint256)' "$MCD_ADM" "$lockAmt"
+        seth send "$MCD_ADM" 'lock(uint256)' "$lockAmt"
 
-        ### Lock Sanity Check
-        lockAmtGtZero=$(echo "$lockAmt > 0" | bc)
-        if [[ "$lockAmtGtZero" == 1 ]]; then
-            seth send "$MCD_GOV" 'approve(address,uint256)' "$MCD_ADM" "$lockAmt"
-            seth send "$MCD_ADM" 'lock(uint256)' "$lockAmt"
-
-            deposits=$(seth call "$MCD_ADM" 'deposits(address)(uint256)' "$ETH_FROM")
-            balanceIOU=$(seth call "$MCD_IOU" 'balanceOf(address)(uint256)' "$ETH_FROM")
-        fi
+        deposits=$(seth call "$MCD_ADM" 'deposits(address)(uint256)' "$ETH_FROM")
     fi
 
     seth send "$MCD_ADM" 'vote(address[] memory)' ["$1"]
@@ -57,18 +43,9 @@ else
 
     seth send "$1" 'cast()'
 
-    ### Check Deposits against Hat Threshold to Free MKR
-    freeAmt=0
-    depositsGtHatThreshold=$(echo "$deposits > $HAT_THRESHOLD" | bc)
-    [[ "$depositsGtHatThreshold" == 1  ]] && freeAmt=$(echo "$deposits - $HAT_THRESHOLD" | bc)
-
-    ### Check IOU Balance
-    balanceIOUGeFreeAmt=$(echo "$balanceIOU >= $freeAmt" | bc)
-    [[ "$balanceIOUGeFreeAmt" == 1 ]] || { echo "$ETH_FROM: Insufficient IOU Balance"; exit 1; }
-
-    ### Free Sanity Check
-    freeAmtGtZero=$(echo "$freeAmt > 0" | bc)
-    if [[ "$freeAmtGtZero" == 1 ]]; then
+    if [[ "$(echo "$deposits > $DESIRED_HAT_APPROVALS" | bc)" == 1 ]]; then
+        freeAmt=$(echo "$deposits - $DESIRED_HAT_APPROVALS" | bc)
+        [[ "$(echo "$(seth call "$MCD_IOU" 'balanceOf(address)(uint256)' "$ETH_FROM") >= $freeAmt" | bc)" == 1 ]] || { echo "$ETH_FROM: Insufficient IOU Balance"; exit 1; }
         seth send "$MCD_IOU" 'approve(address,uint256)' "$MCD_ADM" "$freeAmt"
         seth send "$MCD_ADM" 'free(uint256)' "$freeAmt"
     fi
