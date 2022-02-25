fef46b8efbf043117c213d4083965142a5f5678e
Nazzareno Massari <nas@naszam.co>
2022-02-25 16:06:16 +0000

    optimize script

--- diff-archive-dssspell.sh
+++ diff-archive-dssspell.sh
@@ -4,10 +4,6 @@ set -e
 if [[ -z "$1" ]]; then
   echo "You must provide a date (YYYY-MM-DD) option to diff the directory"
 else
-  diff "./src/Goerli-DssSpell.sol" "./archive/$1-DssSpell/"
-  diff "./src/Goerli-DssSpellCollateralOnboarding.sol" "./archive/$1-DssSpell/"
-  diff "./src/Goerli-DssSpell.t.sol" "./archive/$1-DssSpell/"
-  diff "./src/Goerli-DssSpell.t.base.sol" "./archive/$1-DssSpell/"
-  diff -r "./src/test" "./archive/$1-DssSpell/test"
+  diff -r "./src/" "./archive/$1-DssSpell/"
   echo "Spell, tests and base match the archive directory $1-DssSpell"
 fi
