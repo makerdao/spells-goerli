579ccef0035019f5416d11757b03c8ddfecdded1
Nazzareno Massari <nas@naszam.co>
2022-02-25 19:10:42 +0000

    Optimize `archive-dssspell.sh` (#54)

--- archive-dssspell.sh
+++ archive-dssspell.sh
@@ -4,12 +4,6 @@ set -e
 if [[ -z "$1" ]]; then
   echo "You must provide a date (YYYY-MM-DD) option to name the directory"
 else
-  mkdir "./archive/$1-DssSpell"
-  mkdir "./archive/$1-DssSpell/test"
-  cp "./src/Goerli-DssSpell.sol" "./archive/$1-DssSpell"
-  cp "./src/Goerli-DssSpellCollateralOnboarding.sol" "./archive/$1-DssSpell"
-  cp "./src/Goerli-DssSpell.t.sol" "./archive/$1-DssSpell"
-  cp "./src/Goerli-DssSpell.t.base.sol" "./archive/$1-DssSpell"
-  cp ./src/test/* "./archive/$1-DssSpell/test"
+  cp -r "./src" "./archive/$1-DssSpell"
   echo "Spell, tests and base copied to archive directory $1-DssSpell"
 fi
