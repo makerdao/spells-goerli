8a778c663ebe9a4cd60cb3c481b2e8cd6efc307c
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-03-09 14:57:28 -0300

    Archive spell

---
+++ archive/2022-03-09-DssSpell/Goerli-DssSpell.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (C) 2021-2022 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+// Enable ABIEncoderV2 when onboarding collateral
+//pragma experimental ABIEncoderV2;
+import "dss-exec-lib/DssExec.sol";
+import "dss-exec-lib/DssAction.sol";
+
+import { DssSpellCollateralOnboardingAction } from "./Goerli-DssSpellCollateralOnboarding.sol";
+
+interface FlashKillerLike {
+    function vat() external view returns (address);
+    function flash() external view returns (address);
+}
+
+contract DssSpellAction is DssAction, DssSpellCollateralOnboardingAction {
+    // Provides a descriptive tag for bot consumption
+    string public constant override description = "Goerli Spell";
+
+    address constant FLASH_KILLER = 0xa95FaD7948079df3c579DDb0752E39dC29Eb1AFf;
+
+    // Turn office hours off
+    function officeHours() public override returns (bool) {
+        return false;
+    }
+
+    function actions() public override {
+        require(FlashKillerLike(FLASH_KILLER).vat() == DssExecLib.vat(), "DssSpell/non-matching-vat");
+        address flash = DssExecLib.getChangelogAddress("MCD_FLASH");
+        require(FlashKillerLike(FLASH_KILLER).flash() == flash, "DssSpell/non-matching-flash");
+        DssExecLib.authorize(flash, FLASH_KILLER);
+        DssExecLib.setChangelogAddress("FLASH_KILLER", FLASH_KILLER);
+        // This spell should actually be 1.10.1. However the prev spell bumped to that version (which should have been 1.11.0).
+        // Then now we are going to bump to it (simulating order that is happenning on mainnet)
+        DssExecLib.setChangelogVersion("1.11.0");
+    }
+}
+
+contract DssSpell is DssExec {
+    constructor() DssExec(block.timestamp + 30 days, address(new DssSpellAction())) public {}
+}
