0a4285bebb7855c7b4d9941240fb5539a5fcb41e
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-05-20 10:42:28 -0600

    spell deploy

--- src/test/config.sol
+++ src/test/config.sol
@@ -99,8 +99,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         0,        // use tx=<deploy tx> make deploy-stamp
+            deployed_spell:                 address(0xEf3a5E35cCAAd837781e2cA933EB92E3b3f4BAE5),        // populate with deployed spell if deployed
+            deployed_spell_created:         1653063314,        // use tx=<deploy tx> make deploy-stamp
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
