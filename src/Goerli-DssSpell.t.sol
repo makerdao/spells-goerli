66a82d15eb0663451916c0818d02de18db9db280
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-06-21 18:18:20 -0400

    feat: add starknet contract to chainlog

--- src/Goerli-DssSpell.t.sol
+++ src/Goerli-DssSpell.t.sol
@@ -169,7 +169,7 @@ contract DssSpellTest is GoerliDssSpellTestBase {
         assertTrue(lerp.done());
     }
 
-    function testNewChainlogValues() private { // make public to use
+    function testNewChainlogValues() public {
         vote(address(spell));
         scheduleWaitAndCast(address(spell));
         assertTrue(spell.done());
@@ -177,6 +177,9 @@ contract DssSpellTest is GoerliDssSpellTestBase {
         // Insert new chainlog values tests here
         // checkChainlogKey("CONTRACT_KEY");
         // checkChainlogVersion("X.XX.X");
+
+        checkChainlogKey("STARKNET_CORE");
+        checkChainlogVersion("1.13.2");
     }
 
     function testNewIlkRegistryValues() private { // make public to use
