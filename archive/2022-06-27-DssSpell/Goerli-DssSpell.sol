369474e961505e490ad7ca18648aff04ff4beefe
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-06-27 14:41:03 +0200

    chore: archived spell

---
+++ archive/2022-06-27-DssSpell/Goerli-DssSpell.sol
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: © 2021-2022 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (C) 2021-2022 Dai Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.6.12;
+
+// Enable ABIEncoderV2 when onboarding collateral
+// pragma experimental ABIEncoderV2;
+import "dss-exec-lib/DssExec.sol";
+import "dss-exec-lib/DssAction.sol";
+
+import { DssSpellCollateralAction } from "./Goerli-DssSpellCollateral.sol";
+
+contract DssSpellAction is DssAction, DssSpellCollateralAction {
+
+    // Provides a descriptive tag for bot consumption
+    string public constant override description = "Goerli Spell";
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    // A table of rates can be found at
+    //    https://ipfs.io/ipfs/QmPgPVrVxDCGyNR5rGp9JC5AUxppLzUAqvncRJDcxQnX1u
+    //
+
+    function actions() public override {
+        // ---------------------------------------------------------------------
+        // Includes changes from the DssSpellCollateralOnboardingAction
+        // onboardNewCollaterals();
+        offboardCollaterals();
+
+        // Housekeeping - add Starknet core contract to Chainlog
+        DssExecLib.setChangelogAddress("STARKNET_CORE", 0xde29d060D45901Fb19ED6C6e959EB22d8626708e);
+        DssExecLib.setChangelogVersion("1.13.2");
+    }
+}
+
+contract DssSpell is DssExec {
+    constructor() DssExec(block.timestamp + 30 days, address(new DssSpellAction())) public {}
+}
