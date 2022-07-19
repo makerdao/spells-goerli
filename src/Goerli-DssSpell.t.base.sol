68d5683c9283e58241074bfb36e89a2a6a8e4db6
Henrique Barcelos <16565602+hbarcelos@users.noreply.github.com>
2022-07-19 11:54:47 -0300

    fix: broken FlapLike interface

--- src/Goerli-DssSpell.t.base.sol
+++ src/Goerli-DssSpell.t.base.sol
@@ -51,6 +51,8 @@ interface DirectDepositLike is GemJoinAbstract {
 }
 
 interface FlapLike is FlapAbstract {
+    function fill() external view returns (uint256);
+    function lid() external view returns (uint256);
 }
 
 contract GoerliDssSpellTestBase is Config, DSTest, DSMath {
