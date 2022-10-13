9a23933d774922a3d865d1f74d10a640794c18e0
Nazzareno <nas@naszam.co>
2022-10-13 13:36:17 +0100

    improve url err msg
    Co-authored-by: Gonzalo Balabasquer <gbalabasquer@gmail.com>

--- scripts/hash-exec-copy.sh
+++ scripts/hash-exec-copy.sh
@@ -3,7 +3,7 @@ set -e
 
 URL=$(echo "$1" | cut -f2 -d=)
 
-[[ "$URL" == https://raw.githubusercontent.com/makerdao/community/*/governance/votes/*.md ]] || { echo "Please provide the correct exec copy link to hash (e.g. url=https://raw.githubusercontent.com/makerdao/community/<commit>/governance/votes/*.md)"; exit 1; }
+[[ "$URL" == https://raw.githubusercontent.com/makerdao/community/*/governance/votes/*.md ]] || { echo "Please provide the correct exec copy link to hash (e.g. url=https://raw.githubusercontent.com/makerdao/community/<commit>/governance/votes/<file name>.md)"; exit 1; }
 
 if [[ -x "$(command -v wget)" ]]; then
     cast keccak -- "$(wget "$URL" -q -O - 2>/dev/null)"
