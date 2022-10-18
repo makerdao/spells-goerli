6a7906268afeeaf47fc7c1335b94bc36223b3925
Maciek Kamiński <aundumla@gmail.com>
2022-10-18 12:01:07 +0200

    First draft

--- src/Goerli-DssSpell.sol
+++ src/Goerli-DssSpell.sol
@@ -23,6 +23,19 @@ import "dss-exec-lib/DssAction.sol";
 
 import { DssSpellCollateralAction } from "./Goerli-DssSpellCollateral.sol";
 
+interface StarknetBridgeLike {
+    function close() external;
+}
+
+interface StarknetGovRelayLike {
+    function relay(uint256 spell) external;
+}
+
+interface StarknetEscrowLike {
+    function approve(address token, address spender, uint256 value) external;
+}
+
+
 contract DssSpellAction is DssAction, DssSpellCollateralAction {
     // Provides a descriptive tag for bot consumption
     string public constant override description = "Goerli Spell";
@@ -49,8 +62,8 @@ contract DssSpellAction is DssAction, DssSpellCollateralAction {
     function actions() public override {
         // ---------------------------------------------------------------------
         // rETH Onboarding
-        // https://vote.makerdao.com/polling/QmfMswF2#poll-detail 
-        // https://vote.makerdao.com/polling/QmS7dBuQ#poll-detail 
+        // https://vote.makerdao.com/polling/QmfMswF2#poll-detail
+        // https://vote.makerdao.com/polling/QmS7dBuQ#poll-detail
         // https://forum.makerdao.com/t/reth-collateral-onboarding-risk-evaluation/15286
 
         // Forum
@@ -72,6 +85,29 @@ contract DssSpellAction is DssAction, DssSpellCollateralAction {
         // Includes changes from the DssSpellCollateralAction
         // onboardCollaterals();
         // offboardCollaterals();
+
+        // Starknet Bridge Upgrade
+        // https://github.com/makerdao/starknet-dai-bridge#upgrades
+
+
+        // close current bridge
+        address currentStarknetDAIBridge = DssExecLib.getChangelogAddress("STARKNET_DAI_BRIDGE");
+        StarknetBridgeLike(currentStarknetDAIBridge).close();
+
+        // approve new bridge
+        address NEW_STARKNET_DAI_BRIDGE = 0xaB00D7EE6cFE37cCCAd006cEC4Db6253D7ED3a22;
+        address starknetEscrow = DssExecLib.getChangelogAddress("STARKNET_ESCROW");
+        address dai = DssExecLib.getChangelogAddress("MCD_DAI");
+        StarknetEscrowLike(starknetEscrow).approve(dai, NEW_STARKNET_DAI_BRIDGE, type(uint).max);
+
+        // relay l2 spell
+        address starknetGovRelay = DssExecLib.getChangelogAddress("STARKNET_GOV_RELAY");
+        uint256 L2_FEE_SPELL = 0x04363a4e51a9d2eaccef7a7ef5f0c8872f8183db2179802c0907f547c87864fc;
+        StarknetGovRelayLike(starknetGovRelay).relay(L2_FEE_SPELL);
+
+        // ChangeLog
+        DssExecLib.setChangelogAddress("STARKNET_DAI_BRIDGE", NEW_STARKNET_DAI_BRIDGE);
+        DssExecLib.setChangelogAddress("STARKNET_DAI_BRIDGE_LEGACY", currentStarknetDAIBridge);
     }
 }
 
--- src/test/starknet.t.sol
+++ src/test/starknet.t.sol
@@ -27,6 +27,7 @@ contract ConfigStarknet {
         uint256 dai_bridge_isOpen;
         uint256 dai_bridge_ceiling;
         uint256 dai_bridge_maxDeposit;
+        uint256 l2_dai_bridge;
     }
 
     function setValues() public {
@@ -34,7 +35,8 @@ contract ConfigStarknet {
             core_implementation:       0x60C5fA1763cC9CB9c7c25458C6cDDFbc8F125256,
             dai_bridge_isOpen:         1,        // 1 open, 0 closed
             dai_bridge_ceiling:        200_000,  // Whole Dai Units
-            dai_bridge_maxDeposit:     1000      // Whole Dai Units
+            dai_bridge_maxDeposit:     1000,     // Whole Dai Units
+            l2_dai_bridge:             0x057b7fe4e59d295de5e7955c373023514ede5b972e872e9aa5dcdf563f5cfacb
         });
     }
 }
@@ -58,6 +60,7 @@ interface StarknetDaiBridgeLike {
     function dai() external returns (address);
     function starkNet() external returns (address);
     function escrow() external returns (address);
+    function l2DaiBridge() external returns (uint256);
 }
 
 interface StarknetGovRelayLike {
@@ -124,6 +127,9 @@ contract StarknetTests is GoerliDssSpellTestBase, ConfigStarknet {
 
         assertEq(daiBridge.wards(addr.addr("MCD_PAUSE_PROXY")), 1, "StarknetTest/pause-proxy-not-ward-on-dai-bridge");
         assertEq(daiBridge.wards(addr.addr("MCD_ESM")),         1, "StarknetTest/esm-not-ward-on-dai-bridge");
+
+        uint256 l2DaiBridge = daiBridge.l2DaiBridge();
+        assertEq(daiBridge.l2DaiBridge(), starknetValues.l2_dai_bridge, "StarknetTest/wrong-l2-dai-bridge-on-dai-bridge");
     }
 
     function checkStarknetGovRelay() public {
