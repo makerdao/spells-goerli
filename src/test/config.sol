7ebaa88871d7168065ad1fb02e2e2d201401a592
Arby <nofacedev@protonmail.com>
2022-10-19 19:25:07 +0400

    Removed PSMlike interface from tests file, removed deployed spell address and timestamp from config

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,8 +98,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0x23990e11a1efD07a6000AF0E827A47f2F2086B6E),        // populate with deployed spell if deployed
-            deployed_spell_created:         1666026600,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            deployed_spell:                 address(0),        // populate with deployed spell if deployed
+            deployed_spell_created:         0,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
