9d15d391e11a2beafa807894d7513651d601044b
Arby <nofacedev@protonmail.com>
2022-10-24 19:36:37 +0400

    Added PE_08 deployer to addresses_deployers and reformatted file

--- src/test/addresses_deployers.sol
+++ src/test/addresses_deployers.sol
@@ -32,6 +32,7 @@ contract Deployers {
     address public constant PE_05       = 0xDA01018eA05D98aBb66cb21a85d6019a311570eE;
     address public constant PE_06       = 0xDA0111100cb6080b43926253AB88bE719C60Be13;
     address public constant PE_07       = 0xDa0c0De020F80d43dde58c2653aa73d28Df1fBe1;
+    address public constant PE_08       = 0x39aBD7819E5632Fa06D2ECBba45Dca5c90687EE3;
     address public constant CES_01      = 0x9956fca5a8994737f124c481cEDC6BB3dc5BF010;
     address public constant CES_02      = 0xc0b362cbb0117Ec6A4b589f744d4dECb2768A2eB;
     address public constant CES_03      = 0xb27B6fa77D7FBf3C1BD34B0f7DA59b39D3DB0f7e;
@@ -41,20 +42,22 @@ contract Deployers {
 
     constructor() public {
         addr = [
+            PE_CURRENT,
+            ORACLES,
             PE_01,
             PE_02,
             PE_03,
             PE_04,
             PE_05,
             PE_06,
-            PE_CURRENT,
-            ORACLES,
-            STARKNET_01,
-            STARKNET_02,
+            PE_07,
+            PE_08,
             CES_01,
             CES_02,
             CES_03,
-            CES_04
+            CES_04,
+            STARKNET_01,
+            STARKNET_02
         ];
     }
 
