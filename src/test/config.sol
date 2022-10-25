696dd6b31b62774bb7116c480b2935489300c429
Arby <nofacedev@protonmail.com>
2022-10-25 23:27:05 +0400

    Added deployed spell address and timestamp

--- src/test/config.sol
+++ src/test/config.sol
@@ -98,8 +98,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),        // populate with deployed spell if deployed
-            deployed_spell_created:         0,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            deployed_spell:                 address(0xBe454dE0471c910ee81ADDD4604d9e999E005Fa6),        // populate with deployed spell if deployed
+            deployed_spell_created:         1666725804,        // use `./scripts/get-created-timestamp.sh <deployment-tx>`
             previous_spell:                 address(0),        // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,             // true if officehours is expected to be enabled in the spell
             expiration_threshold:           weekly_expiration  // (weekly_expiration,monthly_expiration) if weekly or monthly spell
