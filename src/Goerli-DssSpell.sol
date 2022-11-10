b78b632cb2b3461f977ef6c80195e0e27db66d70
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-11-10 14:13:01 -0300

    Remove specific spell file for Collaterals

--- src/Goerli-DssSpell.sol
+++ src/Goerli-DssSpell.sol
@@ -21,8 +21,6 @@ pragma solidity 0.6.12;
 import "dss-exec-lib/DssExec.sol";
 import "dss-exec-lib/DssAction.sol";
 
-import { DssSpellCollateralAction } from "./Goerli-DssSpellCollateral.sol";
-
 interface RwaLiquidationLike {
     function ilks(bytes32) external returns (string memory, address, uint48, uint48);
     function init(bytes32, uint256, string calldata, uint48) external;
@@ -32,7 +30,7 @@ interface ChainlogLike {
     function removeAddress(bytes32) external;
 }
 
-contract DssSpellAction is DssAction, DssSpellCollateralAction {
+contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
     string public constant override description = "Goerli Spell";
 
@@ -52,66 +50,16 @@ contract DssSpellAction is DssAction, DssSpellCollateralAction {
     //
 
     // --- Math ---
-    uint256 internal constant WAD = 10 ** 18;
+    uint256 constant WAD            = 10 ** 18;
+    uint256 constant MILLION        = 10 ** 6;
 
-    // Monetalis (RWA007-A) legal update doc
-    string constant RWA007_DOC = "QmejL1CKKN5vCwp9QD1gebnnAM2MJSt9XbF64uy4ptkJtR";
+    uint256 constant FIFTY_PCT_RATE = 1000000012857214317438491659;
 
-    // SG Forge OFH (RWA008-A) legal update doc
-    string constant RWA008_DOC = "QmZ4heYjptvj3ovafADJpXYMFXMyY3yQjkTXpvjFPnAKcy";
+    address immutable MIP21_LIQUIDATION_ORACLE = DssExecLib.getChangelogAddress("MIP21_LIQUIDATION_ORACLE");
 
-    // HVB (RWA009-A) legal update doc
-    string constant RWA009_DOC = "QmeRrbDF8MVPQfNe83gWf2qV48jApVigm1WyjEtDXCZ5rT";
-
-    function actions() public override {
-
-        // Includes changes from the DssSpellCollateralAction
-        collateralAction();
-
-        // -------------------- Update RWA007 Legal Documents ---------------------
-        // TODO: Add forum post link
-        //
-        bytes32 RWA007_ILK = "RWA007-A";
-        updateDoc(RWA007_ILK, RWA007_DOC);
-
-
-        // -------------------- Update RWA008 Legal Documents ---------------------
-        // TODO: Add forum post link
-        //
-        bytes32 RWA008_ILK = "RWA008-A";
-        updateDoc(RWA008_ILK, RWA008_DOC);
-
-        // -------------------- Update RWA009 Legal Documents ---------------------
-        // TODO: Add forum post link
-        //
-        bytes32 RWA009_ILK = "RWA009-A";
-        updateDoc(RWA009_ILK, RWA009_DOC);
-
-        // -------------------- Changelog Housekeeping ---------------------
-        // - Change "RWA007_A_INPUT_CONDUIT_URN" to "RWA007_A_INPUT_CONDUIT"
-        // - Change "RWA007_A_INPUT_CONDUIT_JAR" to "RWA007_A_JAR_INPUT_CONDUIT"
-        //
-        address rwa007inUrn = DssExecLib.getChangelogAddress("RWA007_A_INPUT_CONDUIT_URN");
-        address rwa007inJar = DssExecLib.getChangelogAddress("RWA007_A_INPUT_CONDUIT_JAR");
-
-        DssExecLib.setChangelogAddress("RWA007_A_INPUT_CONDUIT", rwa007inUrn);
-        DssExecLib.setChangelogAddress("RWA007_A_JAR_INPUT_CONDUIT", rwa007inJar);
-        ChainlogLike(DssExecLib.LOG).removeAddress("RWA007_A_INPUT_CONDUIT_URN");
-        ChainlogLike(DssExecLib.LOG).removeAddress("RWA007_A_INPUT_CONDUIT_JAR");
-
-        DssExecLib.setChangelogVersion("1.14.5");
-    }
-
-    function updateDoc(bytes32 ilk, string memory doc) private {
-        address MIP21_LIQUIDATION_ORACLE = DssExecLib.getChangelogAddress(
-            "MIP21_LIQUIDATION_ORACLE"
-        );
-
-        ( , address pip, uint48 tau, ) = RwaLiquidationLike(
-            MIP21_LIQUIDATION_ORACLE
-        ).ilks(ilk);
-
-        require(pip != address(0), "Abort spell execution: pip must be set");
+    function _updateDoc(bytes32 ilk, string memory doc) internal {
+        ( , address pip, uint48 tau, ) = RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).ilks(ilk);
+        require(pip != address(0), "DssSpell/unexisting-rwa-ilk");
 
         // Init the RwaLiquidationOracle to reset the doc
         RwaLiquidationLike(MIP21_LIQUIDATION_ORACLE).init(
@@ -120,6 +68,74 @@ contract DssSpellAction is DssAction, DssSpellCollateralAction {
             doc, // new legal document
             tau         // old tau value
         );
+    }
+
+    function actions() public override {
+        // -------------------- Update RWA007 Legal Documents ---------------------
+        // TODO: Add forum post link
+        //
+        // Monetalis (RWA007-A) legal update doc
+        _updateDoc("RWA007-A", "QmejL1CKKN5vCwp9QD1gebnnAM2MJSt9XbF64uy4ptkJtR");
+
+        // -------------------- Update RWA008 Legal Documents ---------------------
+        // TODO: Add forum post link
+        //
+        // SG Forge OFH (RWA008-A) legal update doc
+        _updateDoc("RWA008-A", "QmZ4heYjptvj3ovafADJpXYMFXMyY3yQjkTXpvjFPnAKcy");
+
+        // -------------------- Update RWA009 Legal Documents ---------------------
+        // TODO: Add forum post link
+        //
+        // HVB (RWA009-A) legal update doc
+        _updateDoc("RWA009-A", "QmeRrbDF8MVPQfNe83gWf2qV48jApVigm1WyjEtDXCZ5rT");
+
+        // RWA007-A autoline changes:
+        // - increase DC to 500m
+        // - increase autoline `gap` to 100m
+        //
+        // https://vote.makerdao.com/polling/QmSfMtTM#poll-detail
+        DssExecLib.setIlkAutoLineParameters(
+            "RWA007-A",
+            500 * MILLION,
+            100 * MILLION,
+            1 weeks
+        );
+
+        // RETH-A autoline changes:
+        // - line 5m
+        // - gap 3m
+        // - ttl 8h
+        //
+        //  https://vote.makerdao.com/polling/QmfMswF2#poll-detail
+        DssExecLib.setIlkAutoLineParameters(
+            "RETH-A",
+            5 * MILLION,
+            3 * MILLION,
+            8 hours
+        );
+
+        // MANA-A param changes:
+        // - decrease line to 3m
+        // - increase SF to 50%
+        // - increase liquidation penalty to 30% from current 13%
+        //
+        // https://forum.makerdao.com/t/mana-a-intermediate-parameter-change-proposal/18727
+        DssExecLib.setIlkAutoLineDebtCeiling("MANA-A", 3 * MILLION);
+        DssExecLib.setIlkStabilityFee("MANA-A", FIFTY_PCT_RATE, true);
+        DssExecLib.setIlkLiquidationPenalty("MANA-A", 3000); // 30%;
+
+        // -------------------- Changelog Update & housekeeping ---------------------
+
+        // - Change "RWA007_A_INPUT_CONDUIT_URN" to "RWA007_A_INPUT_CONDUIT"
+        // - Change "RWA007_A_INPUT_CONDUIT_JAR" to "RWA007_A_JAR_INPUT_CONDUIT"
+        //
+        DssExecLib.setChangelogAddress("RWA007_A_INPUT_CONDUIT", DssExecLib.getChangelogAddress("RWA007_A_INPUT_CONDUIT_URN"));
+        DssExecLib.setChangelogAddress("RWA007_A_JAR_INPUT_CONDUIT", DssExecLib.getChangelogAddress("RWA007_A_INPUT_CONDUIT_JAR"));
+        ChainlogLike(DssExecLib.LOG).removeAddress("RWA007_A_INPUT_CONDUIT_URN");
+        ChainlogLike(DssExecLib.LOG).removeAddress("RWA007_A_INPUT_CONDUIT_JAR");
+
+        // Bump version
+        DssExecLib.setChangelogVersion("1.14.5");
     }
 }
 
