f278afd9d1c51955bd23ce6b776e7f80c1082008
Maciek Kamiński <aundumla@gmail.com>
2022-11-29 15:55:04 +0100

    gov relay upgrade fix

--- src/Goerli-DssSpell.sol
+++ src/Goerli-DssSpell.sol
@@ -34,13 +34,7 @@ contract DssSpellAction is DssAction {
         return false;
     }
 
-    address constant internal MCD_CLIP_CALC_GUSD_A = 0x738EA932C2aFb1D8e47bebB7ed1c604399f2A99e;
-    address constant internal MCD_CLIP_CALC_USDC_A = 0x3a278aA4264AD66c5DEaAfbC1fCf6E43ceD47325;
-    address constant internal MCD_CLIP_CALC_PAXUSD_A = 0x8EE38002052CA938646F653831E9a6Af6Cc8BeBf;
-
-    address immutable internal STARKNET_GOV_RELAY = DssExecLib.getChangelogAddress("STARKNET_GOV_RELAY");
-    address constant internal NEW_STARKNET_GOV_RELAY = 0x8919aefA417745F22c6af5AD6550E83159a373F3;
-    uint256 constant internal L2_GOV_RELAY_SPELL = 0x04c93f9818a4f81f6f2c6f0f660cb4986b789b6b6fb1b274b879649deed74eb8;
+    uint256 constant internal L2_GOV_RELAY_SPELL = TODO;
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -56,108 +50,20 @@ contract DssSpellAction is DssAction {
         // ----------------- Activate Liquidations for GUSD-A, USDC-A and USDP-A -----------------
         // Poll: https://vote.makerdao.com/polling/QmZbsHqu#poll-detail
         // Forum: https://forum.makerdao.com/t/usdc-a-usdp-a-gusd-a-liquidation-parameters-auctions-activation/18744
-        {
-            bytes32 _ilk  = bytes32("GUSD-A");
-            address _clip = DssExecLib.getChangelogAddress("MCD_CLIP_GUSD_A");
-            //
-            // Enable liquidations for GUSD-A
-            // Note: ClipperMom cannot circuit-break on a DS-Value but we're adding
-            //       the rely for consistency with other collaterals and in case the PIP
-            //       changes to an OSM.
-            DssExecLib.authorize(_clip, DssExecLib.clipperMom());
-            DssExecLib.setValue(_clip, "stopped", 0);
-            // Use Abacus/LinearDecrease
-            DssExecLib.setContract(_clip, "calc", MCD_CLIP_CALC_GUSD_A);
-            // Set Liquidation Penalty to 0
-            DssExecLib.setIlkLiquidationPenalty(_ilk, 0);
-            // Set Auction Price Multiplier (buf) to 1
-            DssExecLib.setStartingPriceMultiplicativeFactor(_ilk, 100_00);
-            // Set Local Liquidation Limit (ilk.hole) to 300k DAI
-            DssExecLib.setIlkMaxLiquidationAmount(_ilk, 300_000);
-            // Set tau for Abacus/LinearDecrease to 4,320,000 second
-            DssExecLib.setLinearDecrease(MCD_CLIP_CALC_GUSD_A, 4_320_000);
-            // Set Max Auction Duration (tail) to 43,200 seconds
-            DssExecLib.setAuctionTimeBeforeReset(_ilk, 43_200);
-            // Set Max Auction Drawdown / Permitted Drop (cusp) to 0.99
-            DssExecLib.setAuctionPermittedDrop(_ilk, 99_00);
-            // Set Proportional Kick Incentive (chip) to 0
-            DssExecLib.setKeeperIncentivePercent(_ilk, 0);
-            // Set Flat Kick Incentive (tip) to 0
-            DssExecLib.setKeeperIncentiveFlatRate(_ilk, 0);
-        }
-        {
-            bytes32 _ilk  = bytes32("USDC-A");
-            address _clip = DssExecLib.getChangelogAddress("MCD_CLIP_USDC_A");
-            //
-            // Enable liquidations for USDC-A
-            // Note: ClipperMom cannot circuit-break on a DS-Value but we're adding
-            //       the rely for consistency with other collaterals and in case the PIP
-            //       changes to an OSM.
-            DssExecLib.authorize(_clip, DssExecLib.clipperMom());
-            DssExecLib.setValue(_clip, "stopped", 0);
-            // Use Abacus/LinearDecrease
-            DssExecLib.setContract(_clip, "calc", MCD_CLIP_CALC_USDC_A);
-            // Set Liquidation Penalty to 0
-            DssExecLib.setIlkLiquidationPenalty(_ilk, 0);
-            // Set Auction Price Multiplier (buf) to 1
-            DssExecLib.setStartingPriceMultiplicativeFactor(_ilk, 100_00);
-            // Set Local Liquidation Limit (ilk.hole) to 20m DAI
-            DssExecLib.setIlkMaxLiquidationAmount(_ilk, 20_000_000);
-            // Set tau for Abacus/LinearDecrease to 4,320,000 second
-            DssExecLib.setLinearDecrease(MCD_CLIP_CALC_USDC_A, 4_320_000);
-            // Set Max Auction Duration (tail) to 43,200 seconds
-            DssExecLib.setAuctionTimeBeforeReset(_ilk, 43_200);
-            // Set Max Auction Drawdown / Permitted Drop (cusp) to 0.99
-            DssExecLib.setAuctionPermittedDrop(_ilk, 99_00);
-            // Set Proportional Kick Incentive (chip) to 0
-            DssExecLib.setKeeperIncentivePercent(_ilk, 0);
-            // Set Flat Kick Incentive (tip) to 0
-            DssExecLib.setKeeperIncentiveFlatRate(_ilk, 0);
-        }
-        {
-            bytes32 _ilk  = bytes32("PAXUSD-A");
-            address _clip = DssExecLib.getChangelogAddress("MCD_CLIP_PAXUSD_A");
-            //
-            // Enable liquidations for PAXUSD-A
-            // Note: ClipperMom cannot circuit-break on a DS-Value but we're adding
-            //       the rely for consistency with other collaterals and in case the PIP
-            //       changes to an OSM.
-            DssExecLib.authorize(_clip, DssExecLib.clipperMom());
-            DssExecLib.setValue(_clip, "stopped", 0);
-            // Use Abacus/LinearDecrease
-            DssExecLib.setContract(_clip, "calc", MCD_CLIP_CALC_PAXUSD_A);
-            // Set Liquidation Penalty to 0
-            DssExecLib.setIlkLiquidationPenalty(_ilk, 0);
-            // Set Auction Price Multiplier (buf) to 1
-            DssExecLib.setStartingPriceMultiplicativeFactor(_ilk, 100_00);
-            // Set Local Liquidation Limit (ilk.hole) to 3m DAI
-            DssExecLib.setIlkMaxLiquidationAmount(_ilk, 3_000_000);
-            // Set tau for Abacus/LinearDecrease to 4,320,000 second
-            DssExecLib.setLinearDecrease(MCD_CLIP_CALC_PAXUSD_A, 4_320_000);
-            // Set Max Auction Duration (tail) to 43,200 seconds
-            DssExecLib.setAuctionTimeBeforeReset(_ilk, 43_200);
-            // Set Max Auction Drawdown / Permitted Drop (cusp) to 0.99
-            DssExecLib.setAuctionPermittedDrop(_ilk, 99_00);
-            // Set Proportional Kick Incentive (chip) to 0
-            DssExecLib.setKeeperIncentivePercent(_ilk, 0);
-            // Set Flat Kick Incentive (tip) to 0
-            DssExecLib.setKeeperIncentiveFlatRate(_ilk, 0);
-        }
 
         // ------------------ Setup new Starknet Governance Relay -----------------
         // Forum: https://forum.makerdao.com/t/starknet-changes-for-executive-spell-on-the-week-of-2022-11-29/18818
         // Relay l2 part of the spell
-        // L2 Spell: https://goerli.voyager.online/contract/0x04c93f9818a4f81f6f2c6f0f660cb4986b789b6b6fb1b274b879649deed74eb8#code
+        // L2 Spell:
+        // TODO
+        // it is a fix of 2022-11-28 l2 spell:
+        // https://goerli.voyager.online/contract/0x04c93f9818a4f81f6f2c6f0f660cb4986b789b6b6fb1b274b879649deed74eb8#code
+        // that did not executed correctly due to the incorrect bridge_legacy address:
+        // https://testnet.starkscan.co/tx/0x06057a9ac5d03e09da99c9f07f725af5170eec6d4364f3a7f8dbc03362706698
+
         StarknetGovRelayLike(STARKNET_GOV_RELAY).relay(L2_GOV_RELAY_SPELL);
 
-        // Configure Chainlog
-        DssExecLib.setChangelogAddress("STARKNET_GOV_RELAY_LEGACY", STARKNET_GOV_RELAY);
-        DssExecLib.setChangelogAddress("STARKNET_GOV_RELAY", NEW_STARKNET_GOV_RELAY);
-        DssExecLib.setChangelogAddress("MCD_CLIP_CALC_GUSD_A", MCD_CLIP_CALC_GUSD_A);
-        DssExecLib.setChangelogAddress("MCD_CLIP_CALC_USDC_A", MCD_CLIP_CALC_USDC_A);
-        DssExecLib.setChangelogAddress("MCD_CLIP_CALC_PAXUSD_A", MCD_CLIP_CALC_PAXUSD_A);
-
-        DssExecLib.setChangelogVersion("1.14.6");
+        DssExecLib.setChangelogVersion("1.14.7");
     }
 }
 
