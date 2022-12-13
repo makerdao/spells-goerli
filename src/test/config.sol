315d41703daeeb802154e2a9d314e17e317930f7
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-12-13 14:50:38 -0300

    Test with a deployed empty spell

--- src/test/config.sol
+++ src/test/config.sol
@@ -95,8 +95,8 @@ contract Config {
         // Values for spell-specific parameters
         //
         spellValues = SpellValues({
-            deployed_spell:                 address(0),     // populate with deployed spell if deployed
-            deployed_spell_created:         1670448300,              // use `./scripts/get-created-timestamp.sh <deployment-tx>`
+            deployed_spell:                 address(0x79bCfde936198407b026f432714b03a0DC12368a),     // populate with deployed spell if deployed
+            deployed_spell_created:         1670953596,              // use `./scripts/get-created-timestamp.sh <deployment-tx>`
             previous_spell:                 address(0),     // supply if there is a need to test prior to its cast() function being called on-chain.
             office_hours_enabled:           false,          // true if officehours is expected to be enabled in the spell
             expiration_threshold:           30 days         // Amount of time before spell expires
