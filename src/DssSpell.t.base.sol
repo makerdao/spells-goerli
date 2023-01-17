c81577b2e278f00d5aa28255238d3479e88d17b0
Arby <103920908+The-Arbiter@users.noreply.github.com>
2023-01-17 20:40:10 +0400

    Update src/DssSpell.t.base.sol
    Co-authored-by: Chris Smith <1408372+iamchrissmith@users.noreply.github.com>

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -1107,7 +1107,6 @@ contract DssSpellTestBase is Config, Test {
         amount -= amount * tin / WAD;
         assertEq(token.balanceOf(address(this)), 0, _concat("PSM.sellGem-token-balance-", _ilk));
         assertEq(dai.balanceOf(address(this)), amount * (10 ** (18 - uint256(token.decimals()))), _concat("PSM.sellGem-dai-balance-", _ilk));
-        
         // Convert all DAI to TOKEN (Do not do this if the ilk's DC is 0)
         if (amount > 0) {
             amount -= _divup(amount * tout, WAD);
