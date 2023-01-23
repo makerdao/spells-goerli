dd4f0ad8d4ab3265394d608a93baedc409c1f4a7
Brian L. McMichael <brian@brianmcmichael.com>
2023-01-23 15:01:44 -0500

    Update src/DssSpell.sol
    Co-authored-by: Chris Smith <1408372+iamchrissmith@users.noreply.github.com>

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -45,7 +45,7 @@ contract DssSpellAction is DssAction {
     uint256 internal constant PT_TWO_FIVE_PCT_RATE = 1000000000079175551708715274;
 
     uint256 internal constant MILLION = 10 ** 6;
-    // uint256 internal constant RAY     = 10 ** 27;
+    // uint256 internal constant RAY   = 10 ** 27;
     uint256 internal constant WAD     = 10 ** 18;
 
     ChainLogLike internal immutable CHAINLOG    = ChainLogLike(DssExecLib.getChangelogAddress("CHANGELOG"));
