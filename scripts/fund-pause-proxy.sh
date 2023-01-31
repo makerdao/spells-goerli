d743bc147c429ab3e3e875db79cc966080b6b133
Christopher Mooney <chris@dod.net>
2023-01-31 10:18:50 -0500

    get pause proxy on-chain

--- scripts/fund-pause-proxy.sh
+++ scripts/fund-pause-proxy.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+CHANGELOG="0xdA0Ab1e0017DEbCd72Be8599041a2aa3bA7e740F"
 NETWORK="goerli"
 VALUE="1ether"
 
 [[ "$(cast chain --rpc-url="$ETH_RPC_URL")" == "${NETWORK}" ]] || \
   { echo "Please set a Goerli ETH_RPC_URL"; exit 1; }
 
-MCD_PAUSE_PROXY=$(curl -s https://chainlog.makerdao.com/api/${NETWORK}/active.json | \
-  grep MCD_PAUSE_PROXY | \
-  awk -F"\"" '{print $4}')
+MCD_PAUSE_PROXY=$(cast call "${CHANGELOG}" \
+  'getAddress(bytes32)(address)' \
+  "$(cast --to-bytes32 "$(cast --from-ascii "MCD_PAUSE_PROXY")")")
 
 echo "Pause Proxy ETH balance (wei) before Bombshell: $(cast balance "${MCD_PAUSE_PROXY}")"
 
