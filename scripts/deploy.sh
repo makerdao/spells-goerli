c0d50e69af5686029309658848e918692f2db48d
Nazzareno Massari <nas@naszam.co>
2023-02-20 17:03:37 +0000

    Improve `deploy.sh`

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,11 @@ set -e
 [[ "$(cast chain --rpc-url="$ETH_RPC_URL")" == "goerli" ]] || { echo "Please set a Goerli ETH_RPC_URL"; exit 1; }
 [[ "$ETHERSCAN_API_KEY" ]] || { echo "Please set ETHERSCAN_API_KEY"; exit 1; }
 
-make && \
-  dapp create DssSpell | \
-  xargs ./scripts/verify.py DssSpell
+SOURCE="src/test/config.sol"
+KEY="deployed_spell"
+
+make && spell_address=$(dapp create DssSpell)
+
+./scripts/verify.py DssSpell "$spell_address"
+
+sed -Ei "s/($KEY: *address\()(0x[0-9a-fA-F]{40}\))/\1$spell_address)/" "$SOURCE"
