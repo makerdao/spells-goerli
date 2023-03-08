2114dc1f4a299bee5afec99a76e674b2406f428d
Nazzareno Massari <nas@naszam.co>
2023-03-08 15:28:30 +0000

    add stash/reload feat

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,6 +18,9 @@ make && spell_address=$(dapp create DssSpell)
 
 ./scripts/verify.py DssSpell "$spell_address"
 
+# stash any changes in the staging area
+(set -x; git stash)
+
 # edit config.sol to add the deployed spell address
 sed -Ei "s/($KEY_SPELL: *address\()(0x[[:xdigit:]]{40}|0x0|0)\)/\1$spell_address)/" "$SOURCE"
 
@@ -35,11 +38,16 @@ sed -i "s/\($KEY_BLOCK *: *\)[0-9]\+/\1$block/" "$SOURCE"
 echo -e "${YELLOW}Network: $(cast chain)${NC}"
 echo -e "${YELLOW}config.sol updated with ${PURPLE}deployed spell:${NC} $spell_address, ${PURPLE}timestamp:${NC} $timestamp and ${PURPLE}block:${NC} $block ${NC}"
 
+# add script edit changes
+git add -A
+
+# reload the staging area with stashed changes
+(set -x; git stash apply)
+
 make test block="$block" || { echo -e "${PURPLE}Ensure Tests PASS before commiting the config.sol changes${NC}"; exit 1; }
 
 # commit edit change to config.sol
 if [[ $(git status --porcelain src/test/config.sol) ]]; then
-    (set -x; git add src/test/config.sol)
     (set -x; git commit --edit)
 else
     echo -e "${PURPLE}Ensure config.sol was edited correctly${NC}"
