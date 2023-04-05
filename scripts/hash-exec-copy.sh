0bd24522a9c1fc047668de5d79f6e0045e0f81f9
Nazzareno <nas@naszam.co>
2023-04-05 15:56:49 +0100

    use only curl

--- scripts/hash-exec-copy.sh
+++ scripts/hash-exec-copy.sh
@@ -16,14 +16,14 @@ fi
 # Format input date into GitHub date URL format
 DATE=$(date -d "$1" +"%B %d, %Y" | sed -e 's/ /%20/g' -e 's/,/%2C/g')
 
-if [[ -x "$(command -v wget)" ]] && [[ -x "$(command -v curl)" ]]; then
+if [[ -x "$(command -v curl)" ]]; then
     # Get latest change git commit hash for target exec copy
     commit_hash=$(curl -s "https://api.github.com/repos/makerdao/community/commits?path=governance/votes/Executive%20vote%20-%20$DATE.md&per_page=1" | grep -o -E '"sha": "[^"]+"' | head -n 1 | awk '{print $2}' | sed 's/"//g')
     [[ -z "$commit_hash" ]] && { echo "Error: Executive vote not found"; exit 1; }
     # Hash target exec copy
-    exec_hash=$(cast keccak -- "$(wget https://raw.githubusercontent.com/makerdao/community/"$commit_hash"/governance/votes/Executive%20vote%20-%20"$DATE".md -q -O - 2>/dev/null)")
+    exec_hash=$(cast keccak -- "$(curl -s https://raw.githubusercontent.com/makerdao/community/"$commit_hash"/governance/votes/Executive%20vote%20-%20"$DATE".md)")
 else
-    echo "Please ensure you have curl and wget installed";
+    echo "Please ensure you have curl installed";
     exit 1;
 fi
 
