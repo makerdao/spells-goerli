4e31fc622d2580cc7cd214202b0d6130e633eb8a
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2023-04-07 13:15:23 -0600

    add archive

---
+++ archive/2023-04-07-DssSpell/DssSpell.sol
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: © 2020 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity 0.8.16;
+
+import "dss-exec-lib/DssExec.sol";
+import "dss-exec-lib/DssAction.sol";
+
+contract DssSpellAction is DssAction {
+    // Provides a descriptive tag for bot consumption
+    string public constant override description = "Goerli Spell";
+
+    // Turn office hours off
+    function officeHours() public pure override returns (bool) {
+        return false;
+    }
+
+    // Many of the settings that change weekly rely on the rate accumulator
+    // described at https://docs.makerdao.com/smart-contract-modules/rates-module
+    // To check this yourself, use the following rate calculation (example 8%):
+    //
+    // $ bc -l <<< 'scale=27; e( l(1.08)/(60 * 60 * 24 * 365) )'
+    //
+    // A table of rates can be found at
+    //    https://ipfs.io/ipfs/QmVp4mhhbwWGTfbh2BzwQB9eiBrQBKiqcPRZCaAxNUaar6
+    //
+    // uint256 internal constant X_PCT_RATE      = ;
+
+    address immutable internal ESM            = DssExecLib.getChangelogAddress("MCD_ESM");
+    address immutable internal RWA_LIQ_ORACLE = DssExecLib.getChangelogAddress("MIP21_LIQUIDATION_ORACLE");
+
+    function actions() public override {
+
+        // ----- Additional ESM authorization fix -----
+        DssExecLib.authorize(RWA_LIQ_ORACLE, ESM);
+    }
+}
+
+contract DssSpell is DssExec {
+    constructor() DssExec(block.timestamp + 30 days, address(new DssSpellAction())) {}
+}
