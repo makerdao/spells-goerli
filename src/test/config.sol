c8108b7123b0a5c93393e471b3722f2ae6c966e9
Sweaty Pumpkin <SidestreamSweatyPumpkin@sidestream.tech>
2023-06-05 11:43:55 +0200

    cleanup: config psm-pax-a

--- src/test/config.sol
+++ src/test/config.sol
@@ -1673,9 +1673,9 @@ contract Config {
         });
         afterSpell.collaterals["PSM-PAX-A"] = CollateralValues({
             aL_enabled:   false,
-            aL_line:      0 * MILLION,
-            aL_gap:       0 * MILLION,
-            aL_ttl:       0 hours,
+            aL_line:      0,
+            aL_gap:       0,
+            aL_ttl:       0,
             line:         0,
             dust:         0,
             pct:          0,
