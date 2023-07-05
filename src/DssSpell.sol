08c5af45a7a4cc9740a1c29186232edf5b1c184f
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2023-07-05 10:37:44 +0200

    add available spell content

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -30,10 +30,21 @@ contract DssSpellAction is DssAction {
     // Provides a descriptive tag for bot consumption
     string public constant override description = "Goerli Spell";
 
+    // Always keep office hours off on goerli
+    function officeHours() public pure override returns (bool) {
+        return false;
+    }
+
+    uint256 internal constant THOUSAND    = 10 **  3;
+    uint256 internal constant MILLION     = 10 **  6;
+    uint256 internal constant WAD         = 10 ** 18;
+    uint256 internal constant RAD         = 10 ** 45;
+
     address internal constant MCD_FLAP    = 0x584491031764f94a97a0f98bBe536B004Ab9467b;
     address internal constant FLAPPER_MOM = 0x7316C080BFd1c8857605627a251A2F0ae511E4A1;
     address internal constant PIP_MKR     = 0x496C851B2A9567DfEeE0ACBf04365F3ba00Eb8dC;
-    // NOT FOR GOERLI:
+
+    // NOTE: ignore in goerli
     // address internal constant CRON_SEQUENCER       = ;
     // address internal constant CRON_AUTOLINE_JOB    = ;
     // address internal constant CRON_LERP_JOB        = ;
@@ -41,16 +52,6 @@ contract DssSpellAction is DssAction {
     // address internal constant CRON_CLIPPER_MOM_JOB = ;
     // address internal constant CRON_ORACLE_JOB      = ;
     // address internal constant CRON_FLAP_JOB        = ;
-
-    // Always keep office hours off on goerli
-    function officeHours() public pure override returns (bool) {
-        return false;
-    }
-
-    uint256 internal constant THOUSAND          = 10 **  3;
-    uint256 internal constant MILLION           = 10 **  6;
-    uint256 internal constant WAD               = 10 ** 18;
-    uint256 internal constant RAD               = 10 ** 45;
 
     // Many of the settings that change weekly rely on the rate accumulator
     // described at https://docs.makerdao.com/smart-contract-modules/rates-module
@@ -63,25 +64,20 @@ contract DssSpellAction is DssAction {
     //
     // uint256 internal constant X_PCT_RATE      = ;
 
+    uint256 internal constant THREE_PT_ONE_NINE_PCT_RATE  = 1000000000995743377573746041;
+    uint256 internal constant THREE_PT_FOUR_FOUR_PCT_RATE = 1000000001072474267302354182;
+    uint256 internal constant THREE_PT_NINE_FOUR_PCT_RATE = 1000000001225381266358479708;
+    uint256 internal constant FIVE_PT_SIX_NINE_PCT_RATE   = 1000000001754822903403114680;
+    uint256 internal constant SIX_PT_ONE_NINE_PCT_RATE    = 1000000001904482384730282575;
+    uint256 internal constant FIVE_PT_FOUR_FOUR_PCT_RATE  = 1000000001679727448331902751;
+
     function actions() public override {
         // ----- Deploy Multiswap Conduit for RWA015-A -----
-
-        // ----- Add previous CRON KEYS -----
-
-        // NOT FOR GOERLI
-        // DssExecLib.setChangelogAddress("CRON_SEQUENCER",       CRON_SEQUENCER);
-        // DssExecLib.setChangelogAddress("CRON_AUTOLINE_JOB",    CRON_AUTOLINE_JOB);
-        // DssExecLib.setChangelogAddress("CRON_LERP_JOB",        CRON_LERP_JOB);
-        // DssExecLib.setChangelogAddress("CRON_D3M_JOB",         CRON_D3M_JOB);
-        // DssExecLib.setChangelogAddress("CRON_CLIPPER_MOM_JOB", CRON_CLIPPER_MOM_JOB);
-        // DssExecLib.setChangelogAddress("CRON_ORACLE_JOB",      CRON_ORACLE_JOB);
 
         // ----- Deploy FlapperUniV2 -----
         // Poll: https://vote.makerdao.com/polling/QmQmxEZp#poll-detail
         // Forum: https://forum.makerdao.com/t/introduction-of-smart-burn-engine-and-initial-parameters/21201
         // dss-flappers @ b10f68224c648166cd4f9b09595412bce9824301
-
-        DssExecLib.setChangelogAddress("PIP_MKR", PIP_MKR);
 
         DssInstance memory dss = MCD.loadFromChainlog(DssExecLib.LOG);
         FlapperInstance memory flap = FlapperInstance({
@@ -106,22 +102,85 @@ contract DssSpellAction is DssAction {
             flapper : MCD_FLAP
         });
 
-        // NOT FOR GOERLI:
+        DssExecLib.setChangelogAddress("PIP_MKR", PIP_MKR);
+        // NOTE: ignore in goerli
         // DssCronSequencerLike(CRON_SEQUENCER).addJob(CRON_FLAP_JOB);
         // DssExecLib.setChangelogAddress("CRON_FLAP_JOB", CRON_FLAP_JOB);
+        DssExecLib.setChangelogVersion("1.15.0");
+
+        // ----- Add Cron Jobs to Chainlog -----
+        // Forum: https://forum.makerdao.com/t/dsscron-housekeeping-additions/21292
+        // NOTE: ignore in goerli
+
+        // DssExecLib.setChangelogAddress("CRON_SEQUENCER",       CRON_SEQUENCER);
+        // DssExecLib.setChangelogAddress("CRON_AUTOLINE_JOB",    CRON_AUTOLINE_JOB);
+        // DssExecLib.setChangelogAddress("CRON_LERP_JOB",        CRON_LERP_JOB);
+        // DssExecLib.setChangelogAddress("CRON_D3M_JOB",         CRON_D3M_JOB);
+        // DssExecLib.setChangelogAddress("CRON_CLIPPER_MOM_JOB", CRON_CLIPPER_MOM_JOB);
+        // DssExecLib.setChangelogAddress("CRON_ORACLE_JOB",      CRON_ORACLE_JOB);
 
         // ----- Scope Defined Parameter Changes -----
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-3/21238/6
+
+        // Reduce DSR by 0.30% from 3.49% to 3.19%
+        DssExecLib.setDSR(THREE_PT_ONE_NINE_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce WSTETH-A Liquidation Ratio by 10% from 160% to 150%
+        DssExecLib.setIlkLiquidationRatio("WSTETH-A", 150_00);
+
+        // Reduce WSTETH-B Liquidation Ratio by 10% from 185% to 175%
+        DssExecLib.setIlkLiquidationRatio("WSTETH-B", 175_00);
+
+        // Reduce RETH-A Liquidation Ratio by 20% from 170% to 150%
+        DssExecLib.setIlkLiquidationRatio("RETH-A", 150_00);
+
+        // Reduce the ETH-A Stability Fee (SF) by 0.30% from 3.74% to 3.44%
+        DssExecLib.setIlkStabilityFee("ETH-A", THREE_PT_FOUR_FOUR_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the ETH-B Stability Fee (SF) by 0.30% from 4.24% to 3.94%
+        DssExecLib.setIlkStabilityFee("ETH-B", THREE_PT_NINE_FOUR_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the ETH-C Stability Fee (SF) by 0.30% from 3.49% to 3.19%
+        DssExecLib.setIlkStabilityFee("ETH-C", THREE_PT_ONE_NINE_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the WSTETH-A Stability Fee (SF) by 0.30% from 3.74% to 3.44%
+        DssExecLib.setIlkStabilityFee("WSTETH-A", THREE_PT_FOUR_FOUR_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the WSTETH-B Stability Fee (SF) by 0.30% from 3.49% to 3.19%
+        DssExecLib.setIlkStabilityFee("WSTETH-B", THREE_PT_ONE_NINE_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the RETH-A Stability Fee (SF) by 0.30% from 3.74% to 3.44%
+        DssExecLib.setIlkStabilityFee("RETH-A", THREE_PT_FOUR_FOUR_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the WBTC-A Stability Fee (SF) by 0.11% from 5.80% to 5.69%
+        DssExecLib.setIlkStabilityFee("WBTC-A", FIVE_PT_SIX_NINE_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the WBTC-B Stability Fee (SF) by 0.11% from 6.30% to 6.19%
+        DssExecLib.setIlkStabilityFee("WBTC-B", SIX_PT_ONE_NINE_PCT_RATE, /* doDrip = */ true);
+
+        // Reduce the WBTC-C Stability Fee (SF) by 0.11% from 5.55% to 5.44%
+        DssExecLib.setIlkStabilityFee("WBTC-C", FIVE_PT_FOUR_FOUR_PCT_RATE, /* doDrip = */ true);
 
         // ----- Delegate Compensation for June 2023 -----
+        // NOTE: ignore in goerli
 
         // ----- CRVV1ETHSTETH-A 1st Stage Offboarding -----
+        // NOTE: ignore in goerli
+        // Set CRVV1ETHSTETH-A Debt Ceiling to 0
+        // Remove CRVV1ETHSTETH-A from autoline
 
         // ----- Ecosystem Actor Dai Budget Stream -----
+        // NOTE: ignore in goerli
+        // Chronicle Labs Auditor Wallet | 2023-07-01 00:00:00 to 2024-06-30 23:59:59 | 3,721,800 DAI | 0x68D0ca2d5Ac777F6A9b0d1be44332BB3d5981C2f
 
         // ----- Ecosystem Actor MKR Budget Stream -----
+        // NOTE: ignore in goerli
+        // Chronicle Labs Auditor Wallet | 2023-07-01 00:00:00 to 2024-06-30 23:59:59 | 2,216.4 MKR | 0x68D0ca2d5Ac777F6A9b0d1be44332BB3d5981C2f
 
-
-        DssExecLib.setChangelogVersion("1.15.0");
+        // ----- Core Unit MKR Vesting Transfer -----
+        // Mip: https://mips.makerdao.com/mips/details/MIP40c3SP36#mkr-vesting
+        // NOTE: ignore in goerli
+        // DECO-001 - 125 MKR - 0xF482D1031E5b172D42B2DAA1b6e5Cbf6519596f7
     }
 }
 
--- src/test/config.sol
+++ src/test/config.sol
@@ -111,7 +111,7 @@ contract Config {
         // Values for all system configuration changes
         //
         afterSpell.line_offset =           500 * MILLION;           // Offset between the global line against the sum of local lines
-        afterSpell.pot_dsr =               349;                     // In basis points
+        afterSpell.pot_dsr =               319;                     // In basis points
         afterSpell.pause_delay =           60 seconds;              // In seconds
         afterSpell.vow_wait =              156 hours;               // In seconds
         afterSpell.vow_dump =              250;                     // In whole Dai units
@@ -146,7 +146,7 @@ contract Config {
             aL_ttl:       6 hours,         // In seconds
             line:         0,               // In whole Dai units  // Not checked here as there is auto line
             dust:         7_500,           // In whole Dai units
-            pct:          3_74,            // In basis points
+            pct:          3_44,            // In basis points
             mat:          14500,           // In basis points
             liqType:      "clip",          // "" or "flip" or "clip"
             liqOn:        true,            // If liquidations are enabled
@@ -176,7 +176,7 @@ contract Config {
             aL_ttl:       6 hours,
             line:         0,
             dust:         25 * THOUSAND,
-            pct:          4_24,
+            pct:          3_94,
             mat:          13000,
             liqType:      "clip",
             liqOn:        true,
@@ -206,7 +206,7 @@ contract Config {
             aL_ttl:       8 hours,
             line:         0,
             dust:         3_500,
-            pct:          3_49,
+            pct:          3_19,
             mat:          17000,
             liqType:      "clip",
             liqOn:        true,
@@ -326,7 +326,7 @@ contract Config {
             aL_ttl:       24 hours,
             line:         0,
             dust:         7_500,
-            pct:          5_80,
+            pct:          5_69,
             mat:          14500,
             liqType:      "clip",
             liqOn:        true,
@@ -356,7 +356,7 @@ contract Config {
             aL_ttl:       24 hours,
             line:         0,
             dust:         25 * THOUSAND,
-            pct:          6_30,
+            pct:          6_19,
             mat:          13000,
             liqType:      "clip",
             liqOn:        true,
@@ -386,7 +386,7 @@ contract Config {
             aL_ttl:       24 hours,
             line:         0,
             dust:         3_500,
-            pct:          5_55,
+            pct:          5_44,
             mat:          17500,
             liqType:      "clip",
             liqOn:        true,
@@ -1736,8 +1736,8 @@ contract Config {
             aL_ttl:       12 hours,
             line:         0,
             dust:         7_500,
-            pct:          3_74,
-            mat:          16000,
+            pct:          3_44,
+            mat:          150_00,
             liqType:      "clip",
             liqOn:        true,
             chop:         1300,
@@ -1766,8 +1766,8 @@ contract Config {
             aL_ttl:       16 hours,
             line:         0,
             dust:         3_500,
-            pct:          3_49,
-            mat:          18500,
+            pct:          3_19,
+            mat:          175_00,
             liqType:      "clip",
             liqOn:        true,
             chop:         1300,
@@ -1975,8 +1975,8 @@ contract Config {
             aL_ttl:       8 hours,
             line:         0,
             dust:         7_500,
-            pct:          3_74,
-            mat:          170_00,
+            pct:          3_44,
+            mat:          150_00,
             liqType:      "clip",
             liqOn:        true,
             chop:         13_00,
