66364efea33d7e7d8f140ce30606f59391557251
SidestreamColdMelon <SASCoreUnit+1@gmail.com>
2023-08-16 15:48:00 +0200

    publishing works

--- scripts/cast-on-tenderly/index.js
+++ scripts/cast-on-tenderly/index.js
@@ -18,27 +18,35 @@ if (!SPELL_ADDRESS) {
     throw new Error('Please provide address of the spell, e.g.: `node index.js 0x...`');
 }
 
-const createNewFork = async function () {
-    const response = await axios.post(
-        `https://api.tenderly.co/api/v1/account/${process.env.TENDERLY_USER}/project/${process.env.TENDERLY_PROJECT}/fork`,
-        {
-            network_id: NETWORK_ID,
-        },
+const makeTenderlyApiRequest = async function (path) {
+    const API_BASE = `https://api.tenderly.co/api/v1/account/${process.env.TENDERLY_USER}/project/${process.env.TENDERLY_PROJECT}`;
+    return await axios.post(
+        `${API_BASE}${path}`,
+        { network_id: NETWORK_ID },
         {
             headers: {
                 'X-Access-Key': process.env.TENDERLY_ACCESS_KEY,
             },
         }
     );
+};
+
+const createTenderlyFork = async function () {
+    const response = await makeTenderlyApiRequest('/fork');
     const forkId = response.data.simulation_fork.id;
     const rpcUrl = `https://rpc.tenderly.co/fork/${forkId}`;
     const forkUrl = `https://dashboard.tenderly.co/${process.env.TENDERLY_USER}/${process.env.TENDERLY_PROJECT}/fork/${forkId}`;
-    return { forkUrl, rpcUrl };
+    return { forkId, forkUrl, rpcUrl };
+};
+
+const publishTenderlyTransaction = async function (forkId, transactionId) {
+    await makeTenderlyApiRequest(`/fork/${forkId}/transaction/${transactionId}/share`);
+    return `https://dashboard.tenderly.co/shared/fork/simulation/${transactionId}`;
 };
 
 const runSpell = async function () {
-    const { forkUrl, rpcUrl } = await createNewFork();
-    console.info('tenderly fork is created', forkUrl);
+    const { forkId, forkUrl, rpcUrl } = await createTenderlyFork();
+    console.info('private tenderly fork is created', forkUrl);
 
     const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
     const signer = provider.getSigner();
@@ -57,19 +65,26 @@ const runSpell = async function () {
         throw new Error('spell does not have the hat');
     }
 
-    console.info('scheduling spell on a fork...');
     const spell = new Contract(SPELL_ADDRESS, ['function schedule() external', 'function cast() external'], signer);
-    const scheduleTx = await spell.schedule(DEFAULT_TRANSACTION_PARAMETERS);
-    await scheduleTx.wait();
+    console.info('scheduling spell on a fork...');
+    try {
+        const scheduleTx = await spell.schedule(DEFAULT_TRANSACTION_PARAMETERS);
+        await scheduleTx.wait();
+    } catch (error) {
+        console.warn('scheduling failed', error);
+    }
 
     console.info('warping the time...');
     await provider.send('evm_increaseTime', [ethers.utils.hexValue(60)]);
 
     console.info('casting spell on a fork...');
     const castTx = await spell.cast(DEFAULT_TRANSACTION_PARAMETERS);
-    await castTx.wait();
-
+    const receipt = await castTx.wait();
     console.info('successfully casted');
+
+    const lastTransactionId = await provider.send('evm_snapshot', []);
+    const publicTransactionUrl = await publishTenderlyTransaction(forkId, lastTransactionId);
+    console.info('publicly sharable url', publicTransactionUrl);
 };
 
 runSpell();
