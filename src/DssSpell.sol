98860f8023461b63c62023b225f7ed18af022f4f
0xDecr1pto <0xdecr1pto@proton.me>
2023-10-05 19:43:21 +0300

    feat: add spell actions

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -39,8 +39,97 @@ contract DssSpellAction is DssAction {
     //    https://ipfs.io/ipfs/QmVp4mhhbwWGTfbh2BzwQB9eiBrQBKiqcPRZCaAxNUaar6
     //
     // uint256 internal constant X_PCT_RATE      = ;
+    uint256 internal constant FIVE_PT_TWO_FIVE_PCT_RATE    = 1000000001622535724756171269;
+    uint256 internal constant FIVE_PT_SEVEN_FIVE_PCT_RATE  = 1000000001772819380639683201;
+    uint256 internal constant FIVE_PCT_RATE                = 1000000001547125957863212448;
+    uint256 internal constant FIVE_PT_EIGHT_THREE_PCT_RATE = 1000000001796798806711330296;
+    uint256 internal constant SIX_PT_THREE_THREE_PCT_RATE  = 1000000001946260772914495212;
+    uint256 internal constant FIVE_PT_FIVE_EIGHT_PCT_RATE  = 1000000001721802811203852608;
+
+    //  ---------- Math ----------
+    uint256 internal constant MILLION  = 10 ** 6;
+    uint256 internal constant BILLION  = 10 ** 9;
+
 
     function actions() public override {
+        // ---------- Non-Scope Defined Parameter Changes - WBTC DC-IAM Changes ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-6/22231
+        // Poll: https://vote.makerdao.com/polling/QmNty2pa#poll-detail
+        
+        // Reduce the WBTC-A DC-IAM Target Available Debt from 10 million DAI to 2 million DAI.
+        DssExecLib.setIlkAutoLineParameters("WBTC-A", 500 * MILLION, 2 * MILLION, 24 hours);
+
+        // Reduce the WBTC-B DC-IAM Target Available Debt from 5 million DAI to 2 million DAI.
+        DssExecLib.setIlkAutoLineParameters("WBTC-B", 250 * MILLION, 2 * MILLION, 24 hours);
+
+        // Reduce the WBTC-C DC-IAM Target Available Debt from 10 million DAI to 2 million DAI.
+        DssExecLib.setIlkAutoLineParameters("WBTC-C", 500 * MILLION, 2 * MILLION, 24 hours);
+
+
+        // ---------- Stability Fee Changes ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-6/22231
+
+        // Increase the ETH-A Stability Fee (SF) by 1.55%, from 3.70% to 5.25%.
+        DssExecLib.setIlkStabilityFee("ETH-A", FIVE_PT_TWO_FIVE_PCT_RATE, /* doDrip = */ true);
+
+        // Increase the ETH-B Stability Fee (SF) by 1.55%, from 4.20% to 5.75%.
+        DssExecLib.setIlkStabilityFee("ETH-B", FIVE_PT_SEVEN_FIVE_PCT_RATE, /* doDrip = */ true);
+
+        // Increase the ETH-C Stability Fee (SF) by 1.55%, from 3.45% to 5.00%.
+        DssExecLib.setIlkStabilityFee("ETH-C", FIVE_PCT_RATE, /* doDrip = */ true);
+
+        // Increase WBTC-A Stability Fee (SF) by 0.03%, from 5.8% to 5.83%
+        DssExecLib.setIlkStabilityFee("WBTC-A", FIVE_PT_EIGHT_THREE_PCT_RATE, /* doDrip = */ true);
+
+        // Increase WBTC-B Stability Fee (SF) by 0.03%, from 5.8% to 6.33%
+        DssExecLib.setIlkStabilityFee("WBTC-B", SIX_PT_THREE_THREE_PCT_RATE, /* doDrip = */ true);
+
+        // Increase WBTC-C Stability Fee (SF) by 0.03%, from 5.8% to 5.58%
+        DssExecLib.setIlkStabilityFee("WBTC-C", FIVE_PT_FIVE_EIGHT_PCT_RATE, /* doDrip = */ true);
+
+
+        // ---------- Initial RETH-A Offboarding  ----------
+        // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-6/22231
+        
+        // Set DC-IAM Line (max DC) to 0 (zero). 
+        DssExecLib.setIlkAutoLineDebtCeiling("RETH-A", 0);
+
+
+        // ---------- Reconfiguring Andromeda RWA015-A  ----------
+        // Forum: https://forum.makerdao.com/t/poll-request-reconfiguring-rwa-allocator-vaults/22159
+        // Poll: https://vote.makerdao.com/polling/QmPoLbah
+        
+        // Set the Maximum Debt Ceiling (line) to 3 billion DAI.
+        DssExecLib.setIlkAutoLineDebtCeiling("RWA015-A", 3 * BILLION);
+
+
+        // ---------- Reconfiguring Clydesdale RWA007-A  ----------
+        // Forum: https://forum.makerdao.com/t/poll-request-reconfiguring-rwa-allocator-vaults/22159
+        // Poll: https://vote.makerdao.com/polling/QmPoLbah
+        
+        // Reactivate the Debt Ceiling Instant Access Module for this vault type.
+        // Set the Maximum Debt Ceiling (line) to 3 billion DAI.
+        // Set the Target Available Debt (gap) to 50 million DAI.
+        // Set the Ceiling Increase Cooldown (ttl) to 86400 (24 hours).
+        DssExecLib.setIlkAutoLineParameters("RWA007-A", 3 * BILLION, 50 * MILLION, 24 hours);
+
+        // ---------- Set up Governance Facilitator Streams  ----------
+        // Forum: https://forum.makerdao.com/t/mip102c2-sp16-mip-amendment-subproposal/21579
+        // Poll: https://vote.makerdao.com/polling/QmSovaxn
+        // NOTE: Skip on goerli
+        
+        // JanSky | 2023-10-01 00:00:00 to 2024-09-30 23:59:59 | 504,000.00 DAI | 0xf3F868534FAD48EF5a228Fe78669cf242745a755
+        // VoteWizard | 2023-10-01 00:00:00 to 2024-09-30 23:59:59 | 504,000.00 DAI | 0x9E72629dF4fcaA2c2F5813FbbDc55064345431b1
+        // JanSky | 2023-10-01 00:00:00 to 2024-09-30 23:59:59 | 216.00 MKR | 0xf3F868534FAD48EF5a228Fe78669cf242745a755
+        // VoteWizard | 2023-10-01 00:00:00 to 2024-09-30 23:59:59 | 216.00 MKR | 0x9E72629dF4fcaA2c2F5813FbbDc55064345431b1
+
+
+        // ---------- BA Labs MKR Distribution  ----------
+        // Forum: https://forum.makerdao.com/t/mip40c3-sp25-risk-core-unit-mkr-compensation-risk-001/9788
+        // Poll: https://vote.makerdao.com/polling/QmUAXKm4
+        // NOTE: Skip on goerli
+        
+        // BA Labs - 175 MKR - 0x5d67d5B1fC7EF4bfF31967bE2D2d7b9323c1521c
     }
 }
 
--- src/test/config.sol
+++ src/test/config.sol
@@ -137,7 +137,7 @@ contract Config {
             aL_ttl:       6 hours,         // In seconds
             line:         0,               // In whole Dai units  // Not checked here as there is auto line
             dust:         7_500,           // In whole Dai units
-            pct:          3_70,            // In basis points
+            pct:          5_25,            // In basis points
             mat:          14500,           // In basis points
             liqType:      "clip",          // "" or "flip" or "clip"
             liqOn:        true,            // If liquidations are enabled
@@ -162,7 +162,7 @@ contract Config {
             aL_ttl:       6 hours,
             line:         0,
             dust:         25 * THOUSAND,
-            pct:          4_20,
+            pct:          5_75,
             mat:          13000,
             liqType:      "clip",
             liqOn:        true,
@@ -187,7 +187,7 @@ contract Config {
             aL_ttl:       8 hours,
             line:         0,
             dust:         3_500,
-            pct:          3_45,
+            pct:          5_00,
             mat:          17000,
             liqType:      "clip",
             liqOn:        true,
@@ -283,11 +283,11 @@ contract Config {
         afterSpell.collaterals["WBTC-A"] = CollateralValues({
             aL_enabled:   true,
             aL_line:      500 * MILLION,
-            aL_gap:       10 * MILLION,
+            aL_gap:       2 * MILLION,
             aL_ttl:       24 hours,
             line:         0,
             dust:         7_500,
-            pct:          5_80,
+            pct:          5_83,
             mat:          14500,
             liqType:      "clip",
             liqOn:        true,
@@ -308,11 +308,11 @@ contract Config {
         afterSpell.collaterals["WBTC-B"] = CollateralValues({
             aL_enabled:   true,
             aL_line:      250 * MILLION,
-            aL_gap:       5 * MILLION,
+            aL_gap:       2 * MILLION,
             aL_ttl:       24 hours,
             line:         0,
             dust:         25 * THOUSAND,
-            pct:          6_30,
+            pct:          6_33,
             mat:          13000,
             liqType:      "clip",
             liqOn:        true,
@@ -333,11 +333,11 @@ contract Config {
         afterSpell.collaterals["WBTC-C"] = CollateralValues({
             aL_enabled:   true,
             aL_line:      500 * MILLION,
-            aL_gap:       10 * MILLION,
+            aL_gap:       2 * MILLION,
             aL_ttl:       24 hours,
             line:         0,
             dust:         3_500,
-            pct:          5_55,
+            pct:          5_58,
             mat:          17500,
             liqType:      "clip",
             liqOn:        true,
@@ -1156,11 +1156,11 @@ contract Config {
             offboarding:  false
         });
         afterSpell.collaterals["RWA007-A"] = CollateralValues({
-            aL_enabled:   false,
-            aL_line:      0,
-            aL_gap:       0,
-            aL_ttl:       0,
-            line:         1_250 * MILLION,
+            aL_enabled:   true,
+            aL_line:      3 * BILLION,
+            aL_gap:       50 * MILLION,
+            aL_ttl:       24 hours,
+            line:         0,
             dust:         0,
             pct:          0,
             mat:          10000,
@@ -1357,7 +1357,7 @@ contract Config {
         });
         afterSpell.collaterals["RWA015-A"] = CollateralValues({
             aL_enabled:   true,
-            aL_line:      1_280_000_000,
+            aL_line:      3_000_000_000,
             aL_gap:       50_000_000,
             aL_ttl:       24 hours,
             line:         0,
