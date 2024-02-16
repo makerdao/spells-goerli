0b40f591849a3cadae4e9f4323f2fb2456df0d7b
SidestreamStrongStrawberry <SASCoreUnit+StrongStrawberry@gmail.com>
2024-02-16 15:51:23 +0100

    keep consistency and update section names

--- src/DssSpell.sol
+++ src/DssSpell.sol
@@ -52,10 +52,11 @@ contract DssSpellAction is DssAction {
     uint256 internal constant WAD      = 10 ** 18;
     uint256 internal constant MILLION  = 10 ** 6;
 
+    //  ---------- Addresses ----------
     address internal immutable DIRECT_SPARK_DAI_PLAN = DssExecLib.getChangelogAddress("DIRECT_SPARK_DAI_PLAN");
 
     function actions() public override {
-        // ----- Auction Parameter Updates -----
+        // ---------- Stability Fee Changes ----------
         // Forum: https://forum.makerdao.com/t/stability-scope-parameter-changes-9/23688
 
         // Decrease the ETH-A Stability Fee (SF) by 0.33 percentage points, from 6.74% to 6.41%.
@@ -82,9 +83,9 @@ contract DssSpellAction is DssAction {
         // Decrease the WBTC-C Stability Fee (SF) by 0.02 percentage points, from 6.45% to 6.43%.
         DssExecLib.setIlkStabilityFee("WBTC-C", SIX_PT_FOUR_THREE_PCT_RATE, true);
 
-        // ----- Spark Protocol DC-IAM Parameter Changes (main spell) -----
+        // ---------- Spark Protocol DC-IAM Parameter Changes (main spell) ----------
         // Forum: https://forum.makerdao.com/t/feb-9-2024-proposed-changes-to-sparklend-for-upcoming-spell/23656
-        // Vote: https://vote.makerdao.com/polling/QmS8ch8r
+        // Poll: https://vote.makerdao.com/polling/QmS8ch8r
 
         // Increase the DIRECT-SPARK-DAI Maximum Debt Ceiling (line) by 300 million DAI from 1.2 billion DAI to 1.5 billion DAI.
         // Increase the DIRECT-SPARK-DAI Target Available Debt (gap) by 20 million DAI from 20 million DAI to 40 million DAI.
@@ -92,15 +93,15 @@ contract DssSpellAction is DssAction {
         DssExecLib.setIlkAutoLineParameters("DIRECT-SPARK-DAI", /* line = */ 1500 * MILLION, /* gap = */ 40 * MILLION, /* ttl = */ 24 hours);
 
         // Increase the DIRECT-SPARK-DAI buffer by 20 million DAI from 30 million DAI to 50 million DAI.
-        DssExecLib.setValue(DIRECT_SPARK_DAI_PLAN, "buffer", 50 * MILLION * WAD); // NOTE: adjusting value to DIRECT_SPARK_DAI_PLAN.buffer decimals (18)
+        DssExecLib.setValue(DIRECT_SPARK_DAI_PLAN, "buffer", 50 * MILLION * WAD); // NOTE: adjusting DIRECT_SPARK_DAI_PLAN.buffer value to WAD
 
-        // ----- Push USDP out of input conduit -----
-        // Note: Skipping since there is no Jar for USDP on Goerli
+        // ---------- Push USDP out of input conduit ----------
+        // Note: skipping since there is no Jar for USDP on Goerli
 
-        // ----- yank vest streams -----
+        // ---------- yank vest streams ----------
         // Note: payments are skipped on goerli
 
-        // ----- Trigger Spark Proxy Spell -----
+        // ---------- Trigger Spark Proxy Spell ----------
         // Forum: https://forum.makerdao.com/t/feb-14-2024-proposed-changes-to-sparklend-for-upcoming-spell/23684
         // NOTE: skipped on goerli as spark spell is only deployed to mainnet
     }
